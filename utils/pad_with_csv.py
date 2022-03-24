044325d4d45d7fc71518230037f43958a4093b50
nickjyj <yijun.jiang@lmitechnologies.com>
2022-03-24 12:25:08 -0700

    print warnings

--- utils/pad_with_csv.py
+++ utils/pad_with_csv.py
@@ -22,6 +22,7 @@ def pad_image_with_csv(input_path, csv_path, output_path, output_imsize):
 
     W,H = output_imsize
     cnt = 0
+    cnt_warnings = 0
     output_shapes = {}
     img_paths = glob.glob(os.path.join(input_path, '*.png'))
     for path in img_paths:
@@ -55,8 +56,10 @@ def pad_image_with_csv(input_path, csv_path, output_path, output_imsize):
         shapes = fname_to_shape[im_name]
         #print('[INFO] before: ',[s.up_left+s.bottom_right for s in shapes])
         shapes = fit_shapes_to_size(shapes, pad_l, pad_t)
-        shapes = chop_shapes(shapes, W, H)
+        shapes,is_warning = chop_shapes(shapes, W, H)
         #print('[INFO] after: ',[s.up_left+s.bottom_right for s in shapes])
+        if is_warning:
+            cnt_warnings += 1
         print()
 
         #modify the image name 
@@ -64,6 +67,7 @@ def pad_image_with_csv(input_path, csv_path, output_path, output_imsize):
             shape.im_name = fname
         output_shapes[fname] = shapes
     print(f'[INFO] found {cnt} images with no bbox')
+    print(f'[INFO] found {cnt_warnings} images with bbox that is either removed entirely, or chopped to fit the new size')
     output_csv = os.path.join(output_path, "labels.csv")
     write_to_csv(output_shapes, output_csv)
     
@@ -74,12 +78,14 @@ def chop_shapes(shapes, W, H):
         clip the shapes so that they are fit in the target size [W,H]
     """
     to_del = []
+    is_warning = False
     shapes = np.array(shapes)
     for i,shape in enumerate(shapes):
         x1,y1 = shape.up_left
         x2,y2 = shape.bottom_right
         if x1>=W or y1>=H:
             print(f'[*WARNING] bbox [{x1},{y1},{x2},{y2}] is outside of the size [{W},{H}]')
+            is_warning = True
             to_del.append(i)
         else:
             nx = min(x2,W)
@@ -87,9 +93,11 @@ def chop_shapes(shapes, W, H):
             shapes[i].bottom_right = [nx,ny]
             if nx==W or ny==H:
                 print(f'[*WARNING] bbox [{x1},{y1},{x2},{y2}] is chopped to fit in the size [{W}, {H}]')
+                is_warning = True
     new_shapes = np.delete(shapes,to_del,axis=0)
-    return new_shapes.tolist()
+    return new_shapes.tolist(), is_warning
     
+
 def fit_shapes_to_size(shapes, pad_l, pad_t):
     """
     description:
@@ -105,6 +113,7 @@ def fit_shapes_to_size(shapes, pad_l, pad_t):
         shape.bottom_right[0] += pad_l
         shape.bottom_right[1] += pad_t
     return shapes
+
 
 def fit_array_to_size(im,W,H):
     """
