70d94f4beb65de17c4357fa5ab77b5bf57201401
nickjyj <yijun.jiang@lmitechnologies.com>
2022-04-19 13:28:06 -0700

    modify the plot_labels

--- lmi_utils/plot_labels.py
+++ lmi_utils/plot_labels.py
@@ -3,6 +3,7 @@ import numpy as np
 import random
 import cv2
 import os
+import json
 
 from csv_utils import load_csv
 import rect
@@ -86,13 +87,20 @@ if __name__ == '__main__':
     import argparse
     ap = argparse.ArgumentParser()
     ap.add_argument('--path_imgs', required=True, help='the path to the input image folder')
-    ap.add_argument('--path_csv', default='labels.csv', help='[optinal] the path of a csv file that corresponds to path_imgs, default="labels.csv" in path_imgs')
     ap.add_argument('--path_out', required=True, help='the path to the output folder')
+    ap.add_argument('--path_csv', default='labels.csv', help='[optinal] the path of a csv file that corresponds to path_imgs, default="labels.csv" in path_imgs')
+    ap.add_argument('--class_map_json', default=None, help='[optinal] the path of a class map json file')
     args = vars(ap.parse_args())
 
     path_imgs = args['path_imgs']
     path_csv = args['path_csv'] if args['path_csv']!='labels.csv' else os.path.join(path_imgs, args['path_csv'])
     output_path = args['path_out']
+    if args['class_map_json']:
+        with open(args['class_map_json']) as f:
+            class_map = json.load(f)
+        print(f'loaded class map: {class_map}')
+    else:
+        class_map = None
 
     if not os.path.isfile(path_csv):
         raise Exception(f'Not found file: {path_csv}')
@@ -102,8 +110,19 @@ if __name__ == '__main__':
     if not os.path.isdir(output_path):
         os.makedirs(output_path)
 
-    fname_to_shape, class_map = load_csv(path_csv, path_imgs)
-    color_map = {cls:[random.randint(0, 255) for _ in range(3)] for cls in class_map }
+    fname_to_shape, _ = load_csv(path_csv, path_imgs, class_map)
+    min_id = min(class_map.values())
+    colors = [(0,0,255),(255,0,0),(0,255,0),(102,51,153),(255,140,0),(105,105,105),(127,25,27),(9,200,100)]
+    color_map = {}
+    for cls in class_map:
+        i = class_map[cls]
+        if min_id != 0:
+            i -= 1
+        if i < len(colors):
+            color_map[cls] = colors[i]
+        else:
+            color_map[cls] = tuple([random.randint(0,255) for _ in range(3)])
+
     for im_name in fname_to_shape:
         print(f'[FILE] {im_name}')
         shapes = fname_to_shape[im_name]
