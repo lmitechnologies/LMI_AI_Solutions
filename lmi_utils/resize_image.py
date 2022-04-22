33c5e650f871d71c8c3e537945ee8e4a75d6dd46
nickjyj <yijun.jiang@lmitechnologies.com>
2022-04-22 11:23:36 -0700

    generate output files when run the resize_image.py

--- lmi_utils/resize_image.py
+++ lmi_utils/resize_image.py
@@ -1,53 +1,67 @@
-import numpy as np
+#built-in packages
+import os
+import glob
+
+#3rd party packages
 import cv2
 
-def resize(image, width=None, height=None, inter=cv2.INTER_AREA):
-    '''
-    DESCRIPTION: 
-        resizes images, preserving aspect ratio along argument free dimension
-    ARGS:
-        image: image np array
-        width: desired width
-        height: desired height
-        inter: interpolation method
 
-    '''
-    (h, w) = image.shape[:2]
+def resize_images(path_imgs, output_imsize, path_out):
+    """
+    resize images, while keep the aspect ratio.
+    if the aspect ratio changes, it will generate an error.
+    Arguments:
+        path_imgs(str): the image folder
+        output_imsize(list): a list of output image size [w,h]
+        path_out(str): the output folder
+    Return:
 
-    if (height is None) and (width is None):
-        return image
-    if (height is None) and (width is not None):
-        ratio = width / np.float32(w)
-        height=np.int32(h * ratio)
-    elif (width is None) and (height is not None):
-        ratio = height / np.float32(h)
-        width = np.int32(w * ratio)
-    else:
-        pass
+    """
+    file_list = glob.glob(os.path.join(path_imgs, '*.png'))
+    W,H = output_imsize
+    ratio_out = W/H
+    for file in file_list:
+        im = cv2.imread(file)
+        im_name = os.path.basename(file)
+        h,w = im.shape[:2]
+        print(f'[INFO] Input file: {im_name} with size of [{w},{h}]')
+        
+        ratio_in = w/h
+        assert ratio_in==ratio_out,f'asepect ratio changed from {ratio_in} to {ratio_out}'
+        
+        if im_name.find(str(h)) != -1 and im_name.find(str(w)) != -1:
+            out_name = im_name.replace(str(h), str(H))
+            out_name = out_name.replace(str(w),str(W))
+        else:
+            out_name = os.path.splitext(im_name)[0] + f'_{W}x{H}' + '.png'
+        
+        im2 = cv2.resize(im, dsize=output_imsize)
 
-    resized = cv2.resize(image, (width,height), interpolation=inter)
+        print(f'writting to {out_name}\n')
+        cv2.imwrite(os.path.join(path_out,out_name), im2)
+    return
 
 
-    return resized
 
-
-if __name__=="__main__":
+if __name__=='__main__':
     import argparse
-    ap=argparse.ArgumentParser()
-    ap.add_argument('--input_image_path',required=True)
-    ap.add_argument('--w',type=int,default=256)
-    ap.add_argument('--h',type=int,default=256)
-    args=vars(ap.parse_args())
+    ap = argparse.ArgumentParser()
+    ap.add_argument('--path_imgs', required=True, help='the path to images')
+    ap.add_argument('--out_imsz', required=True, help='the output image size [w,h], w and h are separated by a comma')
+    ap.add_argument('--path_out', required=True, help='the path to resized images')
+    args = vars(ap.parse_args())
 
-    img=cv2.imread(args['input_image_path'])
-    h=args['h']
-    w=args['w']
+    output_imsize = list(map(int,args['out_imsz'].split(',')))
+    assert len(output_imsize)==2, 'the output image size must be two ints'
+    print(f'output image size: {output_imsize}')
     
-    img0=resize(img)
-    img1=resize(img,width=w)
-    img2=resize(img,height=h)
-    img3=resize(img,width=w,height=h)
+    path_imgs = args['path_imgs']
+    path_out = args['path_out']
 
-    print['Done']
+    # create output path
+    assert path_imgs!=path_out, 'input and output path must be different'
+    if not os.path.isdir(path_out):
+        os.makedirs(path_out)
 
-    
+    #resize images with annotation csv file
+    resize_images(path_imgs, output_imsize, path_out)
