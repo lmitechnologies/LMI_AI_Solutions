c29387959e266d2dfc3a79a9b4103797461e4544
Yijun <yijun.jiang@lmitechnologies.com>
2022-04-25 11:33:12 -0400

    fix a bug

--- lmi_utils/via_json_to_csv.py
+++ lmi_utils/via_json_to_csv.py
@@ -8,7 +8,38 @@ import ast
 import glob
 import numpy as np
 
-from resize_image import resize
+
+
+def resize(image, width=None, height=None, inter=cv2.INTER_AREA):
+    '''
+    DESCRIPTION: 
+        resizes images, preserving aspect ratio along argument free dimension
+    ARGS:
+        image: image np array
+        width: desired width
+        height: desired height
+        inter: interpolation method
+
+    '''
+    (h, w) = image.shape[:2]
+
+    if (height is None) and (width is None):
+        return image
+    if (height is None) and (width is not None):
+        ratio = width / np.float32(w)
+        height=np.int32(h * ratio)
+    elif (width is None) and (height is not None):
+        ratio = height / np.float32(h)
+        width = np.int32(w * ratio)
+    else:
+        pass
+
+    resized = cv2.resize(image, (width,height), interpolation=inter)
+
+
+    return resized
+
+
 
 def extract_ROI_from_JSON(data_folder_path,output_csv_file_name,label_name='Name',render=False,is_mask=False,mask_to_bbox=False):
     """
