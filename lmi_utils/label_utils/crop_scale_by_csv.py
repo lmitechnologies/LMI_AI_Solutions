eca8cc0a755edb6ca6f8de4c6574280fe1c56dd2
caaden <chris.aden@lmitechnologies.com>
2022-07-15 14:11:05 -0400

    added crop scale by csv

---
+++ lmi_utils/label_utils/crop_scale_by_csv.py
@@ -0,0 +1,165 @@
+#%% load modules
+import csv
+from eval_utils.iou_from_csv import csv_to_dictionary
+from eval_utils.iou_from_csv import find_class_index
+from label_utils.crop_scale_labeled_image import crop_scale_labeled_image
+import os
+import cv2
+import argparse
+import numpy as np
+
+NAN_INT=-999999
+
+#%% File paths
+def crop_scale(input_data_dir,input_csv_path,output_data_dir,output_csv_path,all_labels,boundingbox_label,object_labels,scl_w,p2w,p2h): 
+    # def_width=False
+    # try:
+    #     scl_w=int(scl_w)
+    #     p2h=int(p2h)
+    #     p2w=int(p2w)
+    # except:
+    #     print('[INFO] Skip rescaling.')
+    #     def_width=True
+    
+    object_classes=object_labels
+    label_dicts=csv_to_dictionary(input_csv_path,all_labels)
+    if not os.path.exists(output_data_dir):
+        os.makedirs(output_data_dir)
+    outfile=output_csv_path
+    # get unique data files
+    files_set=set([item['image_file'] for item in label_dicts])
+    image_files=list(files_set)
+    image_files.sort()
+    with open(outfile,'w',newline='') as csvfile:
+        rowWriter=csv.writer(csvfile,delimiter=';')
+        for image_file in image_files:
+            print('[INFO] converting file: ',image_file)
+            current_labels=[item for item in label_dicts if item['image_file']==image_file]
+            old_image=cv2.imread(os.path.join(input_data_dir,current_labels[0]['image_file']))
+            if boundingbox_label is None:
+                ivec_bbox=[0]
+            else:
+                ivec_bbox=find_class_index(boundingbox_label,current_labels)
+            for i_bbox in ivec_bbox:
+                if boundingbox_label is None:
+                    ul=(0,0)
+                    (Himg,Wimg)=old_image.shape[0:2]
+                    lr=(Wimg,Himg)
+                elif current_labels[i_bbox]['shape']=='rect':
+                    ul=current_labels[i_bbox]['upper_left']
+                    lr=current_labels[i_bbox]['lower_right']
+                else:
+                    raise Exception('Bounding box should be a rect.')
+                bbox=(ul,lr)
+                old_objects=[]
+                object_labels=[]
+                label_types=[]
+                for obj_class in object_classes:
+                    # step through remaining object classes
+                    object_regions=find_class_index(obj_class,current_labels)
+                    # step through split regions, appending all object regions     
+                    for i in object_regions:
+                        if current_labels[i]['shape']=='polygon':
+                            x_vec=current_labels[i]['x_values']
+                            y_vec=current_labels[i]['y_values']
+                            pts=np.stack((x_vec,y_vec),axis=1)
+                            label_types.append('polygon')
+                        elif current_labels[i]['shape']=='rect':
+                            ul=current_labels[i]['upper_left']
+                            lr=current_labels[i]['lower_right']
+                            x_vec=np.asarray([ul[0],lr[0],lr[0],ul[0]])
+                            y_vec=np.asarray([ul[1],ul[1],lr[1],lr[1]])
+                            pts=np.stack((x_vec,y_vec),axis=1)
+                            label_types.append('rect')
+                        else:
+                            raise Exception('Unknown object shape.')
+                        old_objects.append(pts)
+                        object_labels.append(obj_class)
+                # extract new image and new objects
+                new_image,new_objects=crop_scale_labeled_image(old_image,bbox,old_objects,new_width=scl_w,p2h=p2h,p2w=p2w)
+                hout,wout=new_image.shape[:2]         
+                fname=os.path.splitext(image_file)[0]+'_crop_h'+str(hout)+'w'+str(wout)+'.png'
+                fpath=os.path.join(output_data_dir,fname)
+                if os.path.exists(fpath):
+                    fname=os.path.splitext(image_file)[0]+'_crop_h'+str(hout)+'w'+str(wout)+'_v2.png'
+                    fpath=os.path.join(output_data_dir,fname)
+
+                cv2.imwrite(fpath,new_image)
+
+                # step through object classes again, writing new objects
+                for obj_class in object_classes:
+                    # object_regions=object_labels.index(obj_class,current_labels)
+                    object_regions=[i for i,lab in enumerate(object_labels) if lab==obj_class]
+                    for j in object_regions:
+                        label_type=label_types[j]
+                        label=object_labels[j]
+                        xj=list(new_objects[j][:,0])
+                        yj=list(new_objects[j][:,1])
+                        if not (np.any(np.asarray(xj)<NAN_INT) or np.any(np.asarray(yj)<NAN_INT)):
+                            if label_type=='polygon':
+                                rowWriter.writerow([fname,label,'polygon','x values']+xj)
+                                rowWriter.writerow([fname,label,'polygon','y values']+yj)
+                            elif label_type=='rect':
+                                ul=[xj[0],yj[0]]
+                                lr=[xj[2],yj[2]]
+                                rowWriter.writerow([fname,label,'rect','upper left']+ul)
+                                rowWriter.writerow([fname,label,'rect','lower right']+lr)
+                            else:
+                                raise Exception('Unknown object shape.')
+                            # plot 
+                            pts=new_objects[j].astype(np.int32)
+                            cv2.polylines(new_image,[pts],True,(255,0,0),1)
+                            text_ind=pts.min(axis=0)
+                            cv2.putText(new_image,label,(text_ind[0],text_ind[1]-4),cv2.FONT_HERSHEY_SIMPLEX,0.8,(255,0,0),1,cv2.LINE_AA)
+                            cv2.imshow('Validation Image',new_image)
+                            cv2.waitKey(500)
+                fpath=os.path.splitext(fpath)[0]+'_annot.png'
+                cv2.imwrite(fpath,new_image)
+                
+
+if __name__ == '__main__':
+    '''
+    Args:
+        input_data_path: directory of all images
+        input_csv_path:  csv file that includes labels
+        output_data_path: new images
+        output_csv_path: new csv file that has rescaled labels
+        label_defs: string that defines all labeled regions
+        bb_label: label name for outer bounding box
+        object labels: labeled regions of the internal objects
+        scl_w, p2h: new image dimensions
+    '''
+
+    ap=argparse.ArgumentParser()
+    ap.add_argument('--input_data_path',required=True,help='Input data directory.')
+    ap.add_argument('--input_csv_path',required=True,help='Input label.csv path')
+    ap.add_argument('--output_data_path',required=True,help='Output data directory')
+    ap.add_argument('--output_csv_path',required=True,help='Output label.csv path')
+    ap.add_argument('--label_defs',required=True,help='Comma separated list of all label categories.')
+    ap.add_argument('--bb_label',default=None,help='Bounding box label for cropping. (If it exists.)')
+    ap.add_argument('--object_labels',default=None,help='Comma separated list for all labels to keep.')
+    # ap.add_argument('--split_bbw',type=int,default=1)
+    ap.add_argument('--scale_width',type=int,default=None,help='Width of new image.  Scaling will preserve aspect ratio.')
+    ap.add_argument('--pad2width',type=int,default=None,help='Pad to width')
+    ap.add_argument('--pad2height',type=int,default=None,help='Pad to height')
+    args=vars(ap.parse_args())
+
+    idp=args['input_data_path']
+    icsv=args['input_csv_path']
+    odp=args['output_data_path']
+    ocsv=args['output_csv_path']
+    ldef=args['label_defs']
+    bblab=args['bb_label']
+    objects=args['object_labels']
+    scl_w=args['scale_width']
+    p2w=args['pad2width']
+    p2h=args['pad2height']
+    
+    ldef=ldef.split(",")
+    # bblab=bblab.split(",")
+    if objects is not None:
+        mlab=objects.split(",")
+    else:
+        mlab=[]
+
+    crop_scale(idp,icsv,odp,ocsv,ldef,bblab,mlab,scl_w,p2w,p2h)
---
+++ lmi_utils/label_utils/crop_scale_labeled_image.py
@@ -0,0 +1,150 @@
+import cv2
+import numpy as np
+from image_utils.img_resize import resize
+import argparse
+
+def make_border(img,p2h=None,p2w=None):
+    '''
+    Input image is padded on top and bottom to acheive p2h.
+    Input images taller than p2h will be cropped from the top.
+    '''
+
+    (h0,w0)=img.shape[:2]
+    print('[INFO] Make_border() input image dimensions: w=%2d,h=%2d' %(w0,h0))
+
+    top_border=0
+    bottom_border=0
+    left_border=0
+    right_border=0
+
+    if p2h is not None:
+        if h0>p2h:
+            crop_rows=h0-p2h
+            img=img[crop_rows:,:]
+            top_border=-crop_rows
+            bottom_border=top_border+p2h
+            print('[INFO] Scaled image is too tall.  Clipping top %2d rows'%(crop_rows))
+        else:
+            top_border=(p2h-h0)//2
+            bottom_border=p2h-h0-top_border
+            img=cv2.copyMakeBorder(img,top_border,bottom_border,0,0,cv2.BORDER_CONSTANT,value=[0,0,0])
+            print('[INFO] Adding top/bottom borders to maintain image shape.')
+    if p2w is not None:
+        if w0>p2w:
+            crop_cols=w0-p2w
+            img=img[:,crop_cols:]
+            left_border=-crop_cols
+            right_border=left_border+p2w
+            print('[INFO] Scaled image is too wide.  Clipping left %2d rows'%(crop_cols))
+        else:
+            left_border=(p2w-w0)//2
+            right_border=p2w-w0-left_border
+            img=cv2.copyMakeBorder(img,0,0,left_border,right_border,cv2.BORDER_CONSTANT,value=[0,0,0])
+            print('[INFO] Adding left/right borders to maintain image shape.')
+
+    (h1,w1)=img.shape[:2]
+    print('[INFO] New scaled image section shape: W=%2d, H=%2d.'%(w1,h1))
+    return img,top_border,bottom_border,left_border,right_border
+
+
+# bounding box: ((ulx,uly),(lrx,lry))
+def crop_scale_labeled_image(image,boundingbox,masks=None,new_width=None,p2h=None,p2w=None):
+    '''
+    Crops ROI defined by bounding box.
+    Then resizes the image and masks, keeping aspect ratio.
+    Then adds/top bottom borders for fixed shape image.
+    Exceptions when:
+    -mask regions outside of bounding box.
+    -resized region is taller than the new height.
+    '''
+
+    if masks is not None:
+        if not masks:
+            masks=None 
+
+    # extract bounding box coordinates
+    ulx=boundingbox[0][0]
+    uly=boundingbox[0][1]
+    lrx=boundingbox[1][0]
+    lry=boundingbox[1][1]
+
+    # crop image by bounding box
+    cropped_image=image[uly:lry,ulx:lrx]
+    new_image=cropped_image
+    
+    new_masks=[]
+    # recenter masks in cropped image
+    if masks is not None:  
+        for mask in masks:
+            x0=mask[:,0]
+            y0=mask[:,1]
+            x=x0-ulx
+            y=y0-uly
+
+            is_left=np.all(np.less(x0,ulx))
+            is_top=np.all(np.less(y0,uly))
+            is_right=np.all(np.greater(x0,lrx))
+            is_bottom=np.all(np.greater(y0,lry))
+            if (is_left or is_right or is_top or is_bottom):
+                x[:]=np.NaN
+                y[:]=np.NaN
+            else:
+                #crop left,top
+                x[x<0]=0
+                y[y<0]=0
+                #crop right,bottom
+                x[x>lrx-ulx]=lrx-ulx
+                y[y>lry-uly]=lry-uly             
+            new_masks.append(np.stack((x,y),axis=1))
+        masks=new_masks
+
+    # if new_masks<0:
+    #     raise Exception('Mask regions are outside bounding box')
+
+    # rescale image and masks    
+    if new_width is not None:
+        resized_image=resize(cropped_image,width=new_width)
+        new_image=resized_image
+        if masks is not None:
+            (x0,y0)=cropped_image.shape[0:2]
+            (x1,y1)=resized_image.shape[0:2]
+            sclx=x1/x0
+            scly=y1/y0
+            new_masks=[]
+            # scale masks based on image scaling
+            for mask in masks:
+                x=mask[:,0]*sclx
+                # correct invalid mask points
+                # if (np.any(x>new_width)) or (np.any(x<0)):
+                #     print('[INFO] invalid mask point, setting to bounding box edge.')
+                #     x[x>new_width]=0
+                #     x[x<0]=0
+                y=mask[:,1]*scly
+                xy=np.stack((x,y),axis=1).astype(np.int32)
+                new_masks.append(xy)
+    
+    # add borders and shift masks down
+    if (p2h is not None) or (p2w is not None):
+        (h,w)=new_image.shape[:2]
+        new_image,top_border,_,left_border,_=make_border(new_image,p2h,p2w)
+        for i,mask in enumerate(new_masks):
+            mask[:,1]=mask[:,1]+top_border
+            mask[:,1][mask[:,1]<top_border]=top_border
+            mask[:,0]=mask[:,0]+left_border
+            mask[:,0][mask[:,0]<left_border]=left_border
+            new_masks[i]=mask
+        
+    return new_image, new_masks 
+
+
+if __name__=="__main__":
+    ap=argparse.ArgumentParser()
+    ap.add_argument('--image',required=True,help='Base image file.')
+    ap.add_argument('--bounding_box',required=True,help='Bounding box for crop.')
+    ap.add_argument('--masks',default=None,help='Input masks.')
+    ap.add_argument('--new_width',default=None,help='training to validation split.')
+    ap.add_argument('--pad2h',default=None)
+    ap.add_argument('--pad2w',default=None)
+
+    args=vars(ap.parse_args())
+    new_image,new_masks=crop_scale_labeled_image(args['image'],args['bouding_box'],args['masks'],args['new_width'],args['pad2h'],args['pad2w'])
