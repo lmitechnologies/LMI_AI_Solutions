679acf37f73af10061e9f3d56cf465060f85e768
Yijun Jiang <yijun1121@gmail.com>
2022-09-13 18:36:38 -0400

    update the coco_verifier

--- lmi_utils/label_utils/coco_verifier.py
+++ lmi_utils/label_utils/coco_verifier.py
@@ -1,4 +1,5 @@
 import torch
+import os
 
 # Some basic setup:
 # Setup detectron2 logger
@@ -15,7 +16,7 @@ from detectron2.data import MetadataCatalog, DatasetCatalog
 from detectron2.data.datasets import register_coco_instances
 
 
-def plot(path_img, path_json):
+def plot(path_img, path_json, path_out):
     """
     plot to verify if dataset is loaded correctly
     arguments:
@@ -26,21 +27,29 @@ def plot(path_img, path_json):
     dataset_dicts = DatasetCatalog.get("dataset")
     for d in random.sample(dataset_dicts, 10):
         img = cv2.imread(d["file_name"])
-        visualizer = Visualizer(img[:, :, ::-1], metadata=MetadataCatalog.get('dataset'), scale=2)
+        visualizer = Visualizer(img[:, :, ::-1], metadata=MetadataCatalog.get('dataset'), scale=1)
         out = visualizer.draw_dataset_dict(d)
-        cv2.imshow('plot',out.get_image()[:, :, ::-1])
-        cv2.waitKey(0)
-    cv2.destroyAllWindows()
+        im_name = 'annot_'+os.path.basename(d["file_name"])
+        out.save(os.path.join(path_out, im_name))
+        #cv2.imshow('plot',out.get_image()[:, :, ::-1])
+        #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     import argparse
     ap = argparse.ArgumentParser()
     ap.add_argument('--path_img', required=True, type=str, help='the path to the image folder')
-    ap.add_argument('--path_json', required=True, type=str, help='the path to the json annotation file')
-    args = vars(ap.parse_args())
+    ap.add_argument('--path_json', type=str, default='', help='[optinal] the path to the json annotation file, default=labels.json')
+    ap.add_argument('--path_out', required=True, type=str, help='the path to the output folder')
+    args = ap.parse_args()
 
     TORCH_VERSION = ".".join(torch.__version__.split(".")[:2])
     CUDA_VERSION = torch.__version__.split("+")[-1]
     print("torch: ", TORCH_VERSION, "; cuda: ", CUDA_VERSION)
+    
+    if not args.path_json:
+        args.path_json = os.path.join(args.path_img, 'labels.json')
+    if not os.path.isdir(args.path_out):
+        os.makedirs(args.path_out)
 
-    plot(args['path_img'], args['path_json'])
+    plot(args.path_img, args.path_json, args.path_out)
