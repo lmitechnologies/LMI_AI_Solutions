a939ece6d7611b86e280e26b3f8f1aced9e3049c
Yijun Jiang <yijun1121@gmail.com>
2022-09-14 14:47:47 -0400

    add args

--- lmi_utils/pcl_utils/pcd_to_png.py
+++ lmi_utils/pcl_utils/pcd_to_png.py
@@ -1,3 +1,4 @@
+from genericpath import isdir
 import glob
 import os
 import argparse
@@ -5,7 +6,7 @@ import numpy as np
 from pcl_utils.point_cloud import PointCloud
 
 
-def convert_pcd_to_png(path_in,path_out):
+def convert_pcd_to_png(path_in,path_out,color_map,contrast_enhance=False):
 
     files=glob.glob(os.path.join(path_in,'*.pcd'))
 
@@ -14,7 +15,7 @@ def convert_pcd_to_png(path_in,path_out):
     hmin=[]
     for file in files:
         pointcloud.read_points(file,zmin=None,zmax=None,clip_mode=False)
-        pointcloud.convert_points_to_image(color_mapping='gray',contrast_enhancement=True,zmin_color=None,zmax_color=None)
+        pointcloud.convert_points_to_image(color_mapping=color_map,contrast_enhancement=contrast_enhance,zmin_color=None,zmax_color=None)
         file_out=os.path.split(file)[1]
         print(f'[INFO] File: {file_out}, hmin: {pointcloud.zmin}, hmax: {pointcloud.zmax}')
         hmax.append(pointcloud.zmax)
@@ -34,8 +35,16 @@ if __name__=="__main__":
     ap=argparse.ArgumentParser()
     ap.add_argument('-i','--input_path',default='./')
     ap.add_argument('-o','--output_path',default='./')
+    ap.add_argument('--color_map', default='gray',help='the color mapping (gray or rainbow), default=gray')
+    ap.add_argument('--contrast_enhance', action='store_true')
+    
     args=vars(ap.parse_args())
     path_in=args['input_path']
     path_out=args['output_path']
+    color_map = args['color_map']
+    contrast_enhance = args['contrast_enhance']
+    
+    if not os.path.isdir(path_out):
+        os.makedirs(path_out)
 
-    convert_pcd_to_png(path_in,path_out)
+    convert_pcd_to_png(path_in,path_out,color_map,contrast_enhance)
