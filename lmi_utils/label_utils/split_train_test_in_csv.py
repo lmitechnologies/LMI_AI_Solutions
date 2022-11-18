aad16aa192e68162c7b3a0c7a16a32db8fe0d8fb
Yijun Jiang <46984040+nickjyj@users.noreply.github.com>
2022-11-18 14:25:22 -0500

    add an option to generate test data only

--- lmi_utils/label_utils/split_train_test_in_csv.py
+++ lmi_utils/label_utils/split_train_test_in_csv.py
@@ -30,10 +30,11 @@ def split_data_in_csv(path_csv, path_img, test_ratio=0.25, rs=777):
     print(f'split: {len(train_data)} images to training, {len(test_data)} images ({test_ratio*100}%) to testing')
     return train_data,test_data
 
-def generate_split_datasets(train_data, test_data, path_out):
-    path_out_train = os.path.join(path_out, 'train')
-    copy_images(train_data, path_out_train)
-    csv_utils.write_to_csv(train_data, os.path.join(path_out_train, 'labels.csv'))
+def generate_split_datasets(train_data, test_data, path_out, test_only):
+    if not test_only:
+        path_out_train = os.path.join(path_out, 'train')
+        copy_images(train_data, path_out_train)
+        csv_utils.write_to_csv(train_data, os.path.join(path_out_train, 'labels.csv'))
 
     path_out_test = os.path.join(path_out, 'test')
     copy_images(test_data, path_out_test)
@@ -47,6 +48,7 @@ if __name__ == '__main__':
     ap.add_argument('--path_csv', default='labels.csv', help='[optinal] the path of a csv file that corresponds to path_imgs, default="labels.csv" in path_imgs')
     ap.add_argument('--path_out', required=True, help='the output path to the train dataset')
     ap.add_argument('--test_ratio', default=0.2, type=float, help='[optinal] the ratio of the test dataset, default=0.2')
+    ap.add_argument('--test_only', action='store_true', help='[optinal] generate ONLY test dataset')
     args = vars(ap.parse_args())
     
     path_imgs = args['path_imgs']
@@ -55,4 +57,4 @@ if __name__ == '__main__':
     if not os.path.isfile(path_csv):
         raise Exception(f'Not found file: {path_csv}')
     train_data, test_data = split_data_in_csv(path_csv, path_imgs, test_ratio=args['test_ratio'])
-    generate_split_datasets(train_data, test_data, path_out=args['path_out'])
+    generate_split_datasets(train_data, test_data, args['path_out'], test_only=args['test_only'])
