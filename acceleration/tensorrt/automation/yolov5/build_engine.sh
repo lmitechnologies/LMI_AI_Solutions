be73a91847d18fffdad17208ed076da4cc2f2aed
wen.dong <wen.dong@lmi3d.com>
2023-02-01 12:27:50 -0500

    t

---
+++ acceleration/tensorrt/automation/yolov5/build_engine.sh
@@ -0,0 +1,25 @@
+# Compile tensorrtx
+
+BUILD_PATH=/app/build_trt
+mkdir $BUILD_PATH
+
+CONVERSION_NAME="$(date +'%Y-%m-%d-%H-%M')"
+
+OUTPUT_PATH=/app/trt_engines/$CONVERSION_NAME
+mkdir -p $BUILD_PATH
+
+cd "$BUILD_PATH"
+cmake /app/tensorrtx/yolov5
+make
+cd ..
+
+OUT_PREFIX=model
+
+# generate weights
+python3 /app/LMI_AI_Solutions/object_detectors/yolov5/gen_wts.py -w "$WEIGHT_PATH" -o "$OUTPUT_PATH"/"$OUT_PREFIX".wts
+
+# build engine
+"$BUILD_PATH"/yolov5 -c "$CONFIG_PATH" -w "$OUTPUT_PATH"/$OUT_PREFIX.wts -o "$OUTPUT_PATH"/model.engine
+# copy production shared object
+cp $BUILD_PATH/libmyplugins.so $OUTPUT_PATH/libmyplugins.so
+
