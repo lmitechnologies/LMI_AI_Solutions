821ebb9ea5a861ae30a56af9588a158ca4f45914
wen.dong <wen.dong@lmi3d.com>
2023-02-01 19:34:47 -0500

    t

--- acceleration/tensorrt/automation/yolov5/build_engine.sh
+++ acceleration/tensorrt/automation/yolov5/build_engine.sh
@@ -1,8 +1,7 @@
 # Compile tensorrtx
 
-
+# Errors if it's in the dockerfile - Could NOT find CUDA (missing: CUDA_CUDART_LIBRARY) (found version "10.2")
 export CUDA_CUDART_LIBRARY=/usr/local/cuda/lib64/stubs
-
 mkdir -p /app/build_trt && cd /app/build_trt && cmake /app/tensorrtx/yolov5 && make
 
 CONVERSION_NAME="$(date +'%Y-%m-%d-%H-%M')"
