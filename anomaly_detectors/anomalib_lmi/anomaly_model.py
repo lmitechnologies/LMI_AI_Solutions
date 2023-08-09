591af98a7403cebf0dd1a0f6756c1a6236a0e528
caden <caden@lmi3d.com>
2023-08-09 11:01:27 -0700

    simplified predict api

--- anomaly_detectors/anomalib_lmi/anomaly_model.py
+++ anomaly_detectors/anomalib_lmi/anomaly_model.py
@@ -60,22 +60,15 @@ class AnomalyModel:
         self.predict(np.zeros(shape), err_thresh=0)
         logger.info(f"warmup ended - {time.time() - t0}")
 
-    def predict(self, image, err_thresh=0, err_size=0, mask=None):
+    def predict(self, image):
         input_batch = self.preprocess(image)
         self.binding_addrs['input'] = int(input_batch.data_ptr())
         self.context.execute_v2(list(self.binding_addrs.values()))
         outputs = {x:self.bindings[x].data.cpu().numpy() for x in self.output_names}
-        # decision, annot, details = self.postprocess(orig_image=image, 
-        #                                             anomaly_map=outputs['output'], 
-        #                                             err_thresh=err_thresh,
-        #                                             err_size=err_size,
-        #                                             mask=mask)
-        # details.update({k:v for k,v in outputs.items() if k!='output'})
-        # return decision, annot, details
-        return image,outputs['output'],None
+        return outputs['output']
 
         
-    def postprocess(self, orig_image, anomaly_map, err_thresh, err_size, mask):
+    def postprocess(self, orig_image, anomaly_map, err_thresh, err_size, mask=None):
         h,w = orig_image.shape[:2]
         anomaly_map = np.squeeze(anomaly_map.transpose((0,2,3,1)))
         if mask is not None:
