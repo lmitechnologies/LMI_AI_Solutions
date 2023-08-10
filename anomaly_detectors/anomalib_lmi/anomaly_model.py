d871311a0efef94ec230f3c65aba0a99ff2fedd9
Jim Chen <loxijchen@gmail.com>
2023-08-10 13:46:01 -0700

    Finished postprocess and processContours, saving progress

--- anomaly_detectors/anomalib_lmi/anomaly_model.py
+++ anomaly_detectors/anomalib_lmi/anomaly_model.py
@@ -67,8 +67,100 @@ class AnomalyModel:
         outputs = {x:self.bindings[x].data.cpu().numpy() for x in self.output_names}
         return outputs['output']
 
-        
-    def postprocess(self, orig_image, anomaly_map, err_thresh, err_size, mask=None):
+    """
+        DESCRIPTION:
+            processContours
+                - returns decision based off of pass/fail criteria on contour size or data extracted from specific contour regions
+        ARGUMENTS:
+            heatMap:    
+                    - float32 residual_bgr image (heatmap)
+                    - passed in to generate contours
+                    - turned into grayscale, colors are inverted (so the anomaly areas are selected for contour detection,
+                    and then turned into a binary image... excess large white spots are filled in from fill_start to prevent false-positive contours
+                    - resulting image is then passed into findContours, which returns a list of contours
+            err_dist: 
+                    - numpy array (usually the anomaly_map variable)
+                    - error distribution used to extract anomaly data
+                    - a mask of each contour is generated on top of error distribution individually
+                    - Future plan involves using err_dist to generate contours by itself, therefore depreciating the heatMap argument
+            color_threshold: 
+                    - integer
+                    - threshold for generating binary images (minimum darkness before being converted into a black-colored pixel)
+                    - passed into cv2.threshold to convert inverted heatmap (grayscale) into binary image
+            size_fail:
+                    - integer (number of pixels)
+                    - threshold for failing contours
+                    - passed into conditional (if the contour_area is greater than the size_fail, then that anomaly is considered too big to pass)
+            size_ignore:
+                    - integer (number of pixels)
+                    - threshold for ignoring contours
+                    - passed into conditional at the start of contour loop (if contour_area is greater than size_ignore, it's too big to be an actual anomaly)
+                    this assumes detected anomalies are usually a certain size
+        RETURNS:
+            decision:
+                - a (PASS/FAIL) string used in the final decision of the ad model
+            result:
+                - a list of all the contours accepted before FAIL (contains every contour if PASS)
+    """        
+    def processContours(self, heatMap, err_dist, color_threshold, size_fail, size_ignore=None):
+        # preprocess original heatmap image
+        heatmap_gray = cv2.cvtColor(heatMap.astype(np.float32), cv2.COLOR_BGR2GRAY) # turn image into grayscale
+        heatmap_gray_invert = cv2.bitwise_not(heatmap_gray.astype(np.uint8)) # flip the colors
+        _, heat_map_binary = cv2.threshold(heatmap_gray_invert, color_threshold, 255, 0) # turns the flipped grayscale into binary for easier fill
+
+        heat_map_binary_fill = np.copy(heat_map_binary)
+
+        # fill empty white spaces starting from fill_start
+        fill_start = (0,0)
+        floodMask = np.zeros((heat_map_binary.shape[0] + 2, heat_map_binary.shape[1] + 2), dtype=np.uint8)
+        cv2.floodFill(heat_map_binary_fill, floodMask, fill_start, 0, (50, 50, 50))
+
+        contours, _ = cv2.findContours(heat_map_binary_fill.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # finds the contours
+
+        logger.info(f'contours length: {len(contours)}')
+
+        result = []
+
+        for contour in contours:
+            contour_area = cv2.contourArea(contour)
+            logger.info(f'contour area: {contour_area}')
+
+            if size_ignore is not None:
+                if contour_area > size_ignore:
+                    continue
+            
+            result.append(contour)
+
+            """
+            If you wish to extract more data from the contours (mean, max, etc.), uncomment code
+            and add the variables.
+
+            This code below adds a mask over the contour and extracts data strictly from the selected contour region:
+
+                mask = np.zeros_like(heatmap_for_contour).astype(np.uint8)
+                cv2.drawContours(mask, [contour], 0, 255, -1)
+                masked_heatmap = cv2.bitwise_and(heatmap_for_contour.astype(np.uint8), mask)
+                anomaly_scores = masked_heatmap[mask > 0] 
+
+                # mean_score = np.mean(anomaly_scores)
+            """
+            if contour_area > size_fail:
+                return FAIL, result
+
+        return PASS, result
+    
+
+    """
+        DESCRIPTION:
+            postprocess: used to get anomaly decision and resulting image
+        ARGUMENTS:
+            orig_image:
+                - uint16 image (profile image)
+                - used to get original image dimensions for resizing heatmap and/or annotation method
+                - height and width are extracted, then used to resize heatmap/annotated image if processContours or annotate is used 
+            
+    """
+    def postprocess(self, orig_image, anomaly_map, err_thresh, err_size, mask=None, useContours=False, size_ignore=None, size_fail=None, contour_color_threshold=None, useAnnotation=True):
         h,w = orig_image.shape[:2]
         anomaly_map = np.squeeze(anomaly_map.transpose((0,2,3,1)))
         if mask is not None:
@@ -77,20 +169,36 @@ class AnomalyModel:
             anomaly_map = cv2.bitwise_and(anomaly_map_fp32, anomaly_map_fp32, mask=mask)
         ind = anomaly_map<err_thresh
         err_count = np.count_nonzero(ind==False)
-        details = {'emax':round(anomaly_map.max().tolist(), 1), 'ecnt':err_count}
-        if err_count<=err_size:
-            decision=PASS
-            annot=orig_image
+        anomaly_map[ind] = err_thresh
+        total_error = {'emax':round(anomaly_map.max().tolist(), 1), 'ecnt':err_count}
+
+        if useContours:
+            if size_ignore is None or size_fail is None or contour_color_threshold is None:
+                raise ValueError("Required parameters (size_fail & size_ignore) must be provided when useContours=True")
+            heat_map = anomaly_map
+            heat_map_rsz = cv2.resize(heat_map.astype(np.uint8), (w, h))
+            residual_gray = (AnomalyModel.normalize_anomaly_map(heat_map_rsz)*255).astype(np.uint8)
+            residual_bgr = cv2.applyColorMap(np.expand_dims(residual_gray,-1), cv2.COLORMAP_TURBO).astype(np.float32)
+            decision, contours = self.processContours(residual_bgr, anomaly_map, 70, err_thresh)
         else:
-            decision=FAIL
-            anomaly_map[ind] = 0
+            if err_count<=err_size:
+                decision=PASS
+            else:
+                decision=FAIL
+        
+        final_image = orig_image
+        if useAnnotation:
             annot = AnomalyModel.annotate(orig_image.astype(np.uint8), cv2.resize(anomaly_map.astype(np.uint8), (w, h)))
-        cv2.putText(annot,
-                    text=f'ad:{decision},'+ str(details).strip("{}").replace(" ","").replace("\'",""),
-                    org=(4,h-20), fontFace=0, fontScale=1, color=[225, 255, 255],
-                    thickness=2,
-                    lineType=cv2.LINE_AA)
-        return decision, annot, details
+            cv2.putText(annot,
+                        text=f'ad:{decision},'+ str(total_error).strip("{}").replace(" ","").replace("\'",""),
+                        org=(4,h-20), fontFace=0, fontScale=1, color=[225, 255, 255],
+                        thickness=2,
+                        lineType=cv2.LINE_AA)
+            if useContours:
+                cv2.drawContours(annot, contours, -1, (255, 255, 255), 1)
+            final_image = annot
+
+        return decision, final_image, total_error
 
     @staticmethod
     def convert_trt(onnx_path, out_engine_path, fp16=True, workspace=4096):
