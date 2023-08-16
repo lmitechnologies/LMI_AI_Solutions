f8ff7d821567fa54c781aebcf54b6a1119beef84
Yijun Jiang <yijun1121@gmail.com>
2023-08-16 11:24:01 -0700

    add val

--- object_detectors/yolov8_lmi/cmd.py
+++ object_detectors/yolov8_lmi/cmd.py
@@ -13,27 +13,37 @@ logger.setLevel(logging.INFO)
 HYP_YAML = '/app/config/hyp.yaml'
 DATA_YAML = '/app/config/dataset.yaml'
 TRAIN_FOLDER = '/app/training'
+VAL_FOLDER = '/app/validation'
 
 
 if __name__=='__main__':
     # check if files exist
     if not os.path.isfile(HYP_YAML):
         raise FileNotFoundError(f'{HYP_YAML} not found')
-    if not os.path.isfile(DATA_YAML):
-        raise FileNotFoundError(f'{DATA_YAML} not found')
-    
+        
     # load hyp yaml file
     with open(HYP_YAML) as f:
         hyp = yaml.safe_load(f)
+    
+    # check if dataset yaml file exists in the train mode
+    is_train = hyp['mode']=='train'
+    if is_train and not os.path.isfile(DATA_YAML):
+        raise FileNotFoundError(f'{DATA_YAML} not found in the train mode')
+    
     # convert to list of paried strings, such as ['batch=64', 'epochs=100']
     cmd2 = [f'{k}={v}' for k, v in hyp.items()]
     
-    # get the final training cmd
-    today = date.today().strftime("%Y-%m-%d")   # use today's date as the training folder name
-    cmd = ['yolo', f'data={DATA_YAML}', f'project={TRAIN_FOLDER}', f'name={today}']
+    # get the final cmd
+    today = date.today().strftime("%Y-%m-%d")   # use today's date as the output folder name
+    cmd = [
+            'yolo', 
+            f'data={DATA_YAML}' if is_train else '',
+            f'project={TRAIN_FOLDER}' if is_train else f'project={VAL_FOLDER}', 
+            f'name={today}'
+           ]
     cmd += cmd2
     logger.info(f'cmd: {cmd}')
     
-    # run training
+    # run command
     subprocess.run(cmd, check=True)
     
--- object_detectors/yolov8_lmi/x86.dockerfile
+++ object_detectors/yolov8_lmi/x86.dockerfile
@@ -2,13 +2,12 @@
 FROM nvcr.io/nvidia/pytorch:23.04-py3
 ARG DEBIAN_FRONTEND=noninteractive
 
-WORKDIR /repos
-
-# clone AIS
-RUN git clone https://github.com/lmitechnologies/LMI_AI_Solutions.git
-
 # Install dependencies
 RUN apt-get update && apt-get install ffmpeg libsm6 libxext6 -y
 RUN pip install --upgrade pip setuptools wheel
 RUN pip install --user  opencv-python
 RUN pip install ultralytics -U
+
+# clone AIS
+WORKDIR /repos
+RUN git clone https://github.com/lmitechnologies/LMI_AI_Solutions.git
