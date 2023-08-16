1d66ccb7553833a31b2962f73f5c5fd673564327
Yijun Jiang <yijun1121@gmail.com>
2023-08-16 12:23:24 -0700

    t

--- object_detectors/yolov8_lmi/cmd.py
+++ object_detectors/yolov8_lmi/cmd.py
@@ -14,6 +14,8 @@ HYP_YAML = '/app/config/hyp.yaml'
 DATA_YAML = '/app/config/dataset.yaml'
 TRAIN_FOLDER = '/app/training'
 VAL_FOLDER = '/app/validation'
+MODEL_PATH = '/app/trained-inference-models/best.pt'    # for predict and export
+SOURCE_PATH = '/app/data'   # for predict
 
 
 if __name__=='__main__':
@@ -24,14 +26,13 @@ if __name__=='__main__':
     # load hyp yaml file
     with open(HYP_YAML) as f:
         hyp = yaml.safe_load(f)
+    # convert to list of paried strings, such as ['batch=64', 'epochs=100']
+    hyp_cmd = [f'{k}={v}' for k, v in hyp.items()]
     
     # check if dataset yaml file exists in the train mode
     is_train = hyp['mode']=='train'
     if is_train and not os.path.isfile(DATA_YAML):
         raise FileNotFoundError(f'{DATA_YAML} not found in the train mode')
-    
-    # convert to list of paried strings, such as ['batch=64', 'epochs=100']
-    hyp_cmd = [f'{k}={v}' for k, v in hyp.items()]
     
     # get the final cmd
     today = date.today().strftime("%Y-%m-%d")   # use today's date as the output folder name
@@ -42,6 +43,8 @@ if __name__=='__main__':
            ]
     if is_train:
         cmd += [f'data={DATA_YAML}']
+    else:
+        cmd += [f'model={MODEL_PATH}', f'source={SOURCE_PATH}']
     cmd += hyp_cmd
     
     logger.info(f'cmd: {cmd}')
