8b1431a42a7981b4f02d88e95285c52438944357
Yijun Jiang <yijun1121@gmail.com>
2023-08-25 10:47:57 -0400

    t

--- object_detectors/yolov8_lmi/cmd.py
+++ object_detectors/yolov8_lmi/cmd.py
@@ -23,9 +23,9 @@ SOURCE_PATH = '/app/data'
 
 def check_path_exist(path, is_file):
     if is_file and not os.path.isfile(path):
-        raise FileNotFoundError(f'{path} not found')
+        raise Exception(f'Not found: {path}')
     if not is_file and not os.path.isdir(path):
-        raise Exception(f'Path not exists: {path}')
+        raise Exception(f'Not exists: {path}')
     
 def sanity_check(final_configs:dict, check_keys:dict):
     """check if the value to the check_keys exists. If not, throw exception.
