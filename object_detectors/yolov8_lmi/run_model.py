d510ece6c70b36be7d8108b333e63cce53c75ed0
Yijun Jiang <yijun1121@gmail.com>
2023-10-26 09:27:33 -0400

    Squashed commit of the following:
    commit 287f3a904e893b855bdeb0a6a29fd61f984a9109
    Author: Yijun Jiang <yijun1121@gmail.com>
    Date:   Thu Oct 26 09:00:35 2023 -0400
    
        reorgnize code
    
    commit 6ee64076669c6e105fa50319da5581f84b9d6fff
    Author: Yijun Jiang <yijun1121@gmail.com>
    Date:   Wed Oct 25 15:22:30 2023 -0400
    
        fix a bug
    
    commit 49c54bfa22f56727bd84ca37a1976d59c95079fc
    Author: Yijun Jiang <yijun1121@gmail.com>
    Date:   Wed Oct 25 15:20:09 2023 -0400
    
        add log
    
    commit e7d6ea13425981618fbd82327f8b411bd8026d9f
    Author: Yijun Jiang <yijun1121@gmail.com>
    Date:   Wed Oct 25 15:18:52 2023 -0400
    
        test
    
    commit b958a4639eebc3140ac17ee78b49826ecc7bd57d
    Author: Yijun Jiang <yijun1121@gmail.com>
    Date:   Wed Oct 25 15:07:05 2023 -0400
    
        test
    
    commit 212817024d6498fcc74aaf6e49ccfddf3e46dbdb
    Author: Yijun Jiang <yijun1121@gmail.com>
    Date:   Wed Oct 25 15:05:02 2023 -0400
    
        test
    
    commit 07db59377796a45ec844af59692443407df753db
    Author: Yijun Jiang <yijun1121@gmail.com>
    Date:   Wed Oct 25 14:57:45 2023 -0400
    
        add logs
    
    commit 893b125cb25f9ccb493e92936436a4e088cc6eaf
    Author: Yijun Jiang <yijun1121@gmail.com>
    Date:   Wed Oct 25 14:46:31 2023 -0400
    
        add results to csv

--- object_detectors/yolov8_lmi/run_model.py
+++ object_detectors/yolov8_lmi/run_model.py
@@ -4,9 +4,13 @@ import os
 import random
 import numpy as np
 import torch
+import collections
 
 from yolov8_lmi.model import Yolov8
 from gadget_utils.pipeline_utils import plot_one_box, get_img_path_batches
+from label_utils.rect import Rect
+from label_utils.mask import Mask
+from label_utils.csv_utils import write_to_csv
 
 
 BATCH_SIZE = 1
@@ -21,6 +25,7 @@ if __name__ == '__main__':
     parser.add_argument('-o','--path_out' , required=True, help='the path to the output folder')
     parser.add_argument('--sz', required=True, nargs=2, type=int, help='the model input size, two numbers: h w')
     parser.add_argument('-c','--confidence',default=0.25,type=float,help='[optional] the confidence for all classes, default=0.25')
+    parser.add_argument('--csv', action='store_true', help='[optional] whether to save the results to csv file')
     args = parser.parse_args()
     
     logging.basicConfig(level=logging.NOTSET)
@@ -37,35 +42,59 @@ if __name__ == '__main__':
     logger.info(f'warmup input shape: {args.sz}')
     logger.info(f'warmup proc time -> {t2-t1:.4f}')
         
+    fname_to_shapes = collections.defaultdict(list)
     batches = get_img_path_batches(batch_size=BATCH_SIZE, img_dir=args.path_imgs)
     logger.info(f'loaded {len(batches)} with a batch size of {BATCH_SIZE}')
     for batch in batches:
         for p in batch:
             t1 = time.time()
-            
             # inference
             im,im0 = model.load_with_preprocess(p)
             preds = model.forward(im)
             results = model.postprocess(preds,im,im0,args.confidence)
-
-            #annotation
+            t2 = time.time()
+            
             fname = os.path.basename(p)
             save_path = os.path.join(args.path_out,fname)
             im_out = np.copy(im0)
-            for i in range(len(results['boxes'])): # loop through batch
-                # uppack results
-                boxes,scores,classes = results['boxes'][i],results['scores'][i],results['classes'][i]
-                masks = results['masks'][i] if 'masks' in results else None
-                segments = results['segments'][i] if 'segments' in results else []
-                for j in range(len(boxes)-1,-1,-1): # loop through each box
-                    mask = masks[j] if masks is not None else None
-                    plot_one_box(boxes[j],im_out,mask,label=f'{classes[j]}: {scores[j]:.2f}')
-                    if segments and len(segments[j]):
-                        seg = np.array(segments[j]).reshape((-1,1,2)).astype(np.int32)
-                        cv2.drawContours(im_out, [seg], -1, (0, 255, 0), 1)
+            
+            # uppack results for a single image
+            boxes,scores,classes = results['boxes'][0],results['scores'][0],results['classes'][0]
+            masks = results['masks'][0] if 'masks' in results else None
+            segments = results['segments'][0] if 'segments' in results else []
+            
+            # loop through each box
+            for j in range(len(boxes)-1,-1,-1): 
+                mask = masks[j] if masks is not None else None
+                # annotation
+                plot_one_box(boxes[j],im_out,mask,label=f'{classes[j]}: {scores[j]:.2f}')
+                if segments and len(segments[j]):
+                    seg = segments[j].reshape((-1,1,2)).astype(np.int32)
+                    cv2.drawContours(im_out, [seg], -1, (0, 255, 0), 1)
+                    
+                    # add masks to csv
+                    if args.csv:
+                        seg = segments[j].astype(np.int32)
+                        M = Mask(im_name=fname, category=classes[j], x_vals=seg[:,0].tolist(), y_vals=seg[:,1].tolist(), confidence=scores[j])
+                        fname_to_shapes[fname].append(M)
                 
+                # add rects to csv
+                if mask is None and args.csv:
+                    box = boxes[j].astype(np.int32)
+                    R = Rect(im_name=fname, category=classes[j], up_left=box[:2].tolist(), bottom_right=box[2:].tolist(), confidence=scores[j])
+                    fname_to_shapes[fname].append(R)
+                        
             # save output image from RGB to BGR
             cv2.imwrite(save_path,im_out[:,:,::-1])
-                
-            t2 = time.time()
-            logger.info(f'proc time of {fname} -> {t2-t1:.4f}')
+            t3 = time.time()
+            
+            # log
+            cnts = collections.Counter(classes)
+            logger.info(f'fname: {fname}')
+            for c in cnts:
+                logger.info(f'found {cnts[c]} {c}')
+            logger.info(f'proc time: {t2-t1:.4f}, cycle time: {t3-t1:.4f}\n')
+            
+    # write to csv
+    if args.csv:
+        write_to_csv(fname_to_shapes, os.path.join(args.path_out, 'preds.csv'))
