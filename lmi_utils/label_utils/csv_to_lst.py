ad3bca70ce8c97ba677feba89696aa8f0cf43a24
Yijun Jiang <yijun1121@gmail.com>
2024-05-09 12:11:13 -0400

    output xml file

--- lmi_utils/label_utils/csv_to_lst.py
+++ lmi_utils/label_utils/csv_to_lst.py
@@ -1,15 +1,20 @@
 import os
 import json
-import numpy as np
 import cv2
-import collections
+import logging
 
 from label_utils.csv_utils import load_csv
 from label_utils import rect
 from label_utils import mask
 
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
 OUT_NAME = 'lst.json'
+RECT_NAME = 'label'
+POLYGON_NAME = 'polygon'
 
 
 def rect_to_lst(rect_obj, width, height, is_pred):
@@ -28,7 +33,7 @@ def rect_to_lst(rect_obj, width, height, is_pred):
             'rotation': rect_obj.angle,
             'rectanglelabels': [rect_obj.category]
         },
-        'from_name': 'label',
+        'from_name': RECT_NAME,
         'to_name': 'image',
         'type': 'rectanglelabels'
     }
@@ -47,7 +52,7 @@ def mask_to_lst(mask_obj, width, height, is_pred):
                 'points': [],
                 'polygonlabels': [mask_obj.category]
             },
-            'from_name': 'polygon',
+            'from_name': POLYGON_NAME,
             'to_name': 'image',
             'type': 'polygonlabels'
         }
@@ -82,8 +87,46 @@ def init_label_obj(path_img, is_pred):
     return label_obj
 
 
+def write_xml(out_path, box_class, polygon_class):
+    labeling_interface = "<View>\n"
+    labeling_interface += "  <Header value=\"Select label and click the image to start\"/>\n"
+    labeling_interface += "  <Image name=\"image\" value=\"$image\" zoom=\"true\"/>\n"
+    
+    if len(box_class) > 0:
+        labeling_interface += f"  <RectangleLabels name=\"{RECT_NAME}\" toName=\"image\">\n"
+        for name in box_class:
+            labeling_interface += f"    <Label value=\"{name}\" background=\"green\"/>\n"
+        labeling_interface += "  </RectangleLabels>"
+    
+    if len(polygon_class) > 0:
+        labeling_interface += f"  <PolygonLabels name=\"{POLYGON_NAME}\" toName=\"image\" strokeWidth=\"3\" pointSize=\"small\" opacity=\"0.9\">\n"
+        for name in polygon_class:
+            labeling_interface += f"    <Label value=\"{name}\" background=\"blue\"/>\n"
+        labeling_interface += "  </PolygonLabels>\n"
+    
+    labeling_interface += "</View>"
+
+    html_path = f"{out_path}/labeling_interface.xml"
+    with open(html_path, 'w') as file:
+        file.write(labeling_interface)
+
+
 def write_to_lst(shapes, out_path, images_path, gs_path, width, height, is_pred):
+    if not gs_path.startswith('gs://'):
+        if not gs_path.startswith('/'):
+            raise Exception('The local storage path must be absolute path starting with /')
+        l = gs_path.split('/')[1:]
+        logger.info(f'found local path: {gs_path}.')
+        logger.info(f'Assume that LABEL_STUDIO_LOCAL_FILES_DOCUMENT_ROOT is /{l[0]}')
+        gs_path = f"/data/local-files/?d={str('/').join(l[1:])}"
+        logger.info(f'converted to local path: {gs_path}')
+        
     labels = []
+    box_class = set()
+    polygon_class = set()
+    cnt_box = 0
+    cnt_polygon = 0
+    cnt_img = len(shapes)
     for fname in shapes:
         if images_path is not None:
             im = cv2.imread(os.path.join(images_path, fname))
@@ -91,13 +134,18 @@ def write_to_lst(shapes, out_path, images_path, gs_path, width, height, is_pred)
 
         label_obj = init_label_obj(os.path.join(gs_path, fname), is_pred)
         target = 'predictions' if is_pred else 'annotations'
+        
         for shape in shapes[fname]:
             if isinstance(shape, rect.Rect):
                 box = rect_to_lst(shape, width, height, is_pred)
                 label_obj[target][0]['result'].append(box)
+                cnt_box += 1
+                box_class.add(shape.category)
             elif isinstance(shape, mask.Mask):
                 polygon = mask_to_lst(shape, width, height, is_pred)
                 label_obj[target][0]['result'].append(polygon)
+                cnt_polygon += 1
+                polygon_class.add(shape.category)
             else:
                 raise Exception(f'Invalid shape type: {type(shape)}')
         labels.append(label_obj)
@@ -105,21 +153,29 @@ def write_to_lst(shapes, out_path, images_path, gs_path, width, height, is_pred)
     # save to json
     with open(os.path.join(out_path,OUT_NAME), 'w') as f:
         json.dump(labels, f, indent=4)
+        
+    # write the xml file
+    write_xml(out_path, box_class, polygon_class)
+    
+    logger.info(f'Number of images: {cnt_img}')
+    logger.info(f'Number of boxes: {cnt_box}')
+    logger.info(f'Number of polygons: {cnt_polygon}')
+    logger.info(f'In the label studio labeling interface, ensure that PolygonLabels name="{POLYGON_NAME}" and RectangleLabels name="{RECT_NAME}"')
 
 
 if __name__ == '__main__':
     import argparse
     ap = argparse.ArgumentParser()
-    ap.add_argument('--csv', type=str, help='path to the csv file')
-    ap.add_argument('--path_imgs', type=str)
-    ap.add_argument('--wh', type=str, help='width and height of the images separated by a comma. Assume that images are the same size')
-    ap.add_argument('--path_gs', type=str, required=True, help='path to the google storage bucket directory')
-    ap.add_argument('--path_out', '-o', type=str, required=True)
+    ap.add_argument('--csv', required=True, help='path to the csv file')
+    ap.add_argument('--path_imgs', help='[optional] path to the images')
+    ap.add_argument('--wh', help='[optional] width and height of the images separated by a comma. Assume that images are the same size')
+    ap.add_argument('--path_gs', required=True, help='the gs path or local absolute path')
+    ap.add_argument('--path_out', '-o', required=True)
     ap.add_argument('--pred', action='store_true', help='if the csv file is a prediction file')
     args = ap.parse_args()
 
     if args.path_imgs is None and args.wh is None:
-        raise Exception('Provide the path to the images or the width and height of the images')
+        raise Exception('Provide the path to the images. Or if the images are the same size, provide the width and height')
     if args.wh is not None:
         wh = args.wh.split(',')
         width = int(wh[0])
