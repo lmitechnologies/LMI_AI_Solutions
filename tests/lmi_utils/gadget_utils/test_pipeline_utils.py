c09e35b1a4557946e4a03812f4ab5d47b9d8978c
yijun <yijun1121@gmail.com>
2024-07-16 14:34:34 -0400

    update test cases

--- tests/lmi_utils/gadget_utils/test_pipeline_utils.py
+++ tests/lmi_utils/gadget_utils/test_pipeline_utils.py
@@ -19,38 +19,28 @@ logger.setLevel(logging.DEBUG)
 
 
 class Test_resize_image:
-    def test_resize_height(self):
-        im = torch.rand(150, 100, 3).numpy()
-        im2 = pipeline_utils.resize_image(im, H=300)
-        assert im2.shape == (300, 200, 3)
+    @pytest.mark.parametrize(
+        "im, resize_args, expected_shape, expected_type",
+        [
+            (torch.rand(150, 100, 3).numpy(), {"H": 300}, (300, 200, 3), np.ndarray),  # Test resize height
+            (torch.rand(100, 150, 3).numpy(), {"W": 30}, (20, 30, 3), np.ndarray),    # Test resize width
+            (torch.rand(150, 100, 3).numpy(), {"W": 200, "H": 200}, (200, 200, 3), np.ndarray),  # Test warp
+            (torch.rand(150, 100).numpy(), {"W": 50}, (75, 50), np.ndarray),  # Test gray image (2D)
+            (torch.rand(150, 100, 1).numpy(), {"W": 50}, (75, 50, 1), np.ndarray),  # Test gray image (3D)
+        ]
+    )
+    def test_cases(self,im, resize_args, expected_shape, expected_type):
+        im2 = pipeline_utils.resize_image(im, **resize_args)
+        assert im2.shape == expected_shape
+        assert type(im2) == expected_type
         
-    def test_resize_width(self):
-        im = torch.rand(100, 150, 3).numpy()
-        im2 = pipeline_utils.resize_image(im, W=30)
-        assert im2.shape == (20, 30, 3)
-        
-    def test_warp(self):
-        im = torch.rand(150, 100, 3).numpy()
-        im2 = pipeline_utils.resize_image(im, 200, 200)
-        assert im2.shape == (200, 200, 3)
-        
-    def test_gray_image(self):
-        im = torch.rand(150, 100).numpy()
-        im2 = pipeline_utils.resize_image(im, W=50)
-        assert im2.shape == (75, 50)
-        
-        im = torch.rand(150, 100, 1).numpy()
-        im2 = pipeline_utils.resize_image(im, W=50)
-        assert im2.shape == (75, 50, 1)
-        assert type(im2) == np.ndarray
     
-    def test_tensor_cpu(self):
+    def test_tensors(self):
         im  = torch.rand(100, 150, 3)
         im2 = pipeline_utils.resize_image(im, H=10)
         assert im2.shape == (10, 15, 3)
         assert type(im2) == torch.Tensor
         
-    def test_tensor_gpu(self):
         if torch.cuda.is_available():
             im  = torch.rand(100, 100, 3).cuda()
             im2 = pipeline_utils.resize_image(im, W=150)
@@ -60,62 +50,34 @@ class Test_resize_image:
 
             
 class Test_fit_im_to_size:
-    def test_pad_w(self):
-        im = torch.rand(100, 100, 3).numpy()
-        im2,l,r,t,b = pipeline_utils.fit_im_to_size(im, W=121)
-        assert l==10 and r==11 and t==0 and b==0
-        assert im2.shape == (100, 121, 3)
-    
-    def test_pad_h(self):
-        im = torch.rand(100, 100, 3).numpy()
-        im2,l,r,t,b = pipeline_utils.fit_im_to_size(im, H=131)
-        assert l==0 and r==0 and t==15 and b==16
-        assert im2.shape == (131, 100, 3)
+    @pytest.mark.parametrize(
+        "im, wh, expected_pad, expected_shape",
+        [
+            (torch.rand(100, 100, 3).numpy(), [121, None], [10, 11, 0, 0], (100, 121, 3)),
+            (torch.rand(100, 100, 3).numpy(), [None, 131], [0, 0, 15, 16], (131, 100, 3)),
+            (torch.rand(100, 100, 3).numpy(), [151, 131], [25, 26, 15, 16], (131, 151, 3)),
+            (torch.rand(100, 100, 3).numpy(), [71, 75], [-14, -15, -12, -13], (75, 71, 3)),
+            (torch.rand(100, 100, 3).numpy(), [None, 89], [0, 0, -5, -6], (89, 100, 3)),
+            (torch.rand(100, 100, 3).numpy(), [89, None], [-5, -6, 0, 0], (100, 89, 3)),
+            (torch.rand(100, 100).numpy(), [89, None], [-5, -6, 0, 0], (100, 89)),
+            (torch.rand(100, 100, 1).numpy(), [89, None], [-5, -6, 0, 0], (100, 89, 1)),
+        ]
+    )
+    def test_cases(self, im, wh, expected_pad, expected_shape):
+        W,H = wh
+        im2, l, r, t, b = pipeline_utils.fit_im_to_size(im, W=W, H=H)
+        assert np.array_equal([l,r,t,b], expected_pad)
+        assert im2.shape == expected_shape
+        assert isinstance(im2, np.ndarray)
         
-    def test_pad_hw(self):
-        im = torch.rand(100, 100, 3).numpy()
-        im2,l,r,t,b = pipeline_utils.fit_im_to_size(im, W=151, H=131)
-        assert l==25 and r==26 and t==15 and b==16
-        assert im2.shape == (131, 151, 3)
-        
-    def test_crop_hw(self):
-        im = torch.rand(100, 100, 3).numpy()
-        im2,l,r,t,b = pipeline_utils.fit_im_to_size(im, W=71, H=75)
-        assert l==-14 and r==-15 and t==-12 and b==-13
-        assert im2.shape == (75, 71, 3)
-        
-    def test_crop_h(self):
-        im = torch.rand(100, 100, 3).numpy()
-        im2,l,r,t,b = pipeline_utils.fit_im_to_size(im, H=89)
-        assert l==0 and r==0 and t==-5 and b==-6
-        assert im2.shape == (89, 100, 3)
-        
-    def test_crop_w(self):
-        im = torch.rand(100, 100, 3).numpy()
-        im2,l,r,t,b = pipeline_utils.fit_im_to_size(im, W=89)
-        assert l==-5 and r==-6 and t==0 and b==0
-        assert im2.shape == (100, 89, 3)
-        
-    def test_gray_image(self):
-        im = torch.rand(100, 100).numpy()
-        im2,l,r,t,b = pipeline_utils.fit_im_to_size(im, W=89)
-        assert l==-5 and r==-6 and t==0 and b==0
-        assert im2.shape == (100, 89)
-        
-        im = torch.rand(100, 100, 1).numpy()
-        im2,l,r,t,b = pipeline_utils.fit_im_to_size(im, W=89)
-        assert l==-5 and r==-6 and t==0 and b==0
-        assert im2.shape == (100, 89, 1)
-        assert type(im2) == np.ndarray
-        
-    def test_tensor_cpu(self):
+    def test_tensors(self):
+        # cpu
         im = torch.rand(100, 100, 3)
         im2,l,r,t,b = pipeline_utils.fit_im_to_size(im, W=89)
         assert l==-5 and r==-6 and t==0 and b==0
         assert im2.shape == (100, 89, 3)
         assert type(im2) == torch.Tensor
         
-    def test_tensor_gpu(self):
         if torch.cuda.is_available():
             im = torch.rand(100, 100, 3).cuda()
             im2,l,r,t,b = pipeline_utils.fit_im_to_size(im, W=89)
@@ -135,27 +97,30 @@ class Test_fit_im_to_size:
             assert l == l2 and r == r2 and t == t2 and b == b2
         
 
-class Test_revert_to_origin:
-    def test_1(self):
-        pts = [[10,20],[30,40],[50,60],[70,80]]
-        operations = [{'resize':[100,100,200,300]},{'pad':[8,9,10,11]},{'stretch':[1.5,2]}]
+class Test_revert_to_origin:        
+    @pytest.mark.parametrize(
+        "pts, operations, expected",
+        [
+            (
+                [[10, 20], [30, 40], [50, 60], [70, 80]],
+                [{'resize': [100, 100, 200, 300]}, {'pad': [8, 9, 10, 11]}, {'stretch': [1.5, 2]}],
+                np.array([[-2.66, 0.0], [24.0, 30.0], [50.66, 60.0], [77.34, 90.0]])
+            ),
+            (
+                np.array([[15, 25], [35, 45], [55, 65], [75, 85]]),
+                [{'resize': [200, 300, 100, 100]}, {'pad': [8, 9, 10, 11]}, {'stretch': [1.5, 2]}],
+                np.array([[1.0, 0.83], [7.67, 4.17], [14.34, 7.5], [21.0, 10.83]])
+            ),
+            (
+                [[15, 25, 35, 45], [55, 65, 75, 85]],
+                [{'resize': [200, 300, 100, 100]}, {'pad': [8, 9, 10, 11]}, {'stretch': [1.5, 2]}],
+                np.array([[1.0, 0.83, 7.67, 4.17], [14.34, 7.5, 21.0, 10.83]])
+            ),
+        ]
+    )
+    def test_cases(self, pts, operations, expected):
         pts2 = pipeline_utils.revert_to_origin(pts, operations)
-        ans = np.array([[-2.66, 0.0], [24.0, 30.0], [50.66, 60.0], [77.34, 90.0]])
-        assert np.array_equal(pts2, ans.round().clip(0))
-    
-    def test_2(self):
-        pts = np.array([[15,25],[35,45],[55,65],[75,85]])
-        operations = [{'resize':[200,300,100,100]},{'pad':[8,9,10,11]},{'stretch':[1.5,2]}]
-        pts2 = pipeline_utils.revert_to_origin(pts, operations)
-        ans = np.array([[1.0, 0.83], [7.67, 4.17], [14.34, 7.5], [21.0, 10.83]])
-        assert np.array_equal(pts2, ans.round())
-
-    def test_3(self):
-        pts = [[15, 25, 35, 45], [55, 65, 75, 85]]
-        operations = [{'resize':[200,300,100,100]},{'pad':[8,9,10,11]},{'stretch':[1.5,2]}]
-        pts2 = pipeline_utils.revert_to_origin(pts, operations)
-        ans = np.array([[1.0, 0.83, 7.67, 4.17], [14.34, 7.5, 21.0, 10.83]])
-        assert np.array_equal(pts2, ans.round())
+        assert np.array_equal(pts2, expected.round().clip(0))
         
     def test_types(self):
         pts = np.array([[15, 25, 35, 45], [55, 65, 75, 85]])
