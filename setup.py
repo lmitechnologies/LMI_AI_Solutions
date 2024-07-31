99d135c7318940deb4cb5a4bae9ff7203678e5ba
RugvedMavidipalli <rmavidipalli@gmail.com>
2024-07-31 17:32:14 -0400

    exposing github url

--- setup.py
+++ setup.py
@@ -3,14 +3,15 @@ from setuptools import setup, find_packages
 # Read the contents of your README file
 from pathlib import Path
 this_directory = Path(__file__).parent
-# long_description = (this_directory / "README.md").read_text()
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='lmi-aisolutions',  
     version='1.0.0',  
     description='LMI AI Solutions Package',  
-    # long_description=long_description,
+    long_description=long_description,
     long_description_content_type='text/markdown',
+    url='https://github.com/lmitechnologies/LMI_AI_Solutions.git',
     license='MIT',  
     packages=find_packages(),
     keywords='LMI Technologies LMI AI Solutions',  
