9e8bafe1cf6c2f8e66372eab516005243a763901
RugvedMavidipalli <rmavidipalli@gmail.com>
2024-08-16 14:44:03 -0400

    support to combine csv files

---
+++ lmi_utils/label_utils/combine_csv.py
@@ -0,0 +1,23 @@
+from label_utils import csv_utils
+import os
+import argparse
+import glob
+
+def combine_csv(csv_file_list, output_dir):
+    all_shapes = {}
+    for csv_file in csv_file_list:
+        shapes, _ = csv_utils.load_csv(csv_file)
+        all_shapes.update(shapes)
+    csv_utils.write_to_csv(f"{os.path.join(output_dir, "combined.csv")}", all_shapes)
+    return all_shapes
+
+def main():
+    parser = argparse.ArgumentParser(description='Combine multiple csv files')
+    parser.add_argument('--input-dir', type=str, help='Input directory containing csv files')
+    parser.add_argument('--output-dir', type=str, help='Output directory to save the combined csv file')
+    args = parser.parse_args()
+    csv_files = glob.glob(f"{args.input_dir}/*.csv")
+    combine_csv(csv_files, args.output_dir)
+    
+if __name__ in '__main__':
+    main()
