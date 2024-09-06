8b2af298d255c929ff49e4360c6085d8ab9bcddd
RugvedMavidipalli <rmavidipalli@gmail.com>
2024-09-06 10:35:56 -0400

    organized the scripts

--- object_detectors/detectron2/utils/utils.py
+++ object_detectors/detectron2/utils/utils.py
@@ -104,34 +104,3 @@ def create_config(cfg_file_path):
     return cfg
 
 
-def train_model(cfg):
-    # train the model
-    trainer = DefaultTrainer(cfg)
-    trainer.resume_or_load(resume=False)
-    trainer.train()
-
-
-def main(args):
-    dataset_dir = args.dataset_dir
-    config_file = args.config_file
-    # # check if the dataset directory exists
-    # if not os.path.exists(dataset_dir):
-    #     raise ValueError(f"Dataset directory {dataset_dir} does not exist")
-    # # get the training and test data directories
-    # train_data_dir = os.path.join(dataset_dir, "train")
-    # val_data_dir = os.path.join(dataset_dir, "val")
-    # test_data_dir = os.path.join(dataset_dir, "test")
-    # # register the datasets
-    # register_datasets(train_data_dir, val_data_dir, test_data_dir)
-    # # create the config
-    # cfg = create_config(config_file)
-    # # train the model
-    # train_model(cfg)
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--dataset-dir", type=str, help="Path to the dataset directory")
-    parser.add_argument("--config-file", type=str, help="Path to the config file")
-    args = parser.parse_args()
-    main()
