d539379cd7fb40afe5fbe9c4930d5b6c0b49f92e
RugvedMavidipalli <rmavidipalli@gmail.com>
2024-09-06 12:26:51 -0400

    updated datasets registeration

--- object_detectors/detectron2_lmi/trainer.py
+++ object_detectors/detectron2_lmi/trainer.py
@@ -1,4 +1,5 @@
 from detectron2.engine import DefaultTrainer
+from detectron2.data.datasets import register_coco_instances
 import argparse
 import os
 from utils.det_utils import register_datasets, create_config
@@ -18,11 +19,18 @@ def main(args):
     cfg, original_config = create_config(config_file)
     # register the datasets train, test
     for dataset_name, dataset_path in zip(original_config['DATASETS']["TRAIN"], original_config['DATASETS']["TRAIN_DIR"]):
-        print(f"Registering dataset {dataset_name} from {dataset_path}")
-        register_datasets(dataset_name, dataset_path)
-    for dataset_name, dataset_path in zip(original_config['DATASETS']["TEST"], original_config['DATASETS']["TEST_DIR"]):
-        print(f"Registering dataset {dataset_name} from {dataset_path}")
-        register_datasets(dataset_name, dataset_path)
+        annot_file = os.path.join(dataset_path, "annotations.json")
+        images_path = os.path.join(dataset_path, "images")
+        register_coco_instances(
+                dataset_name,
+                {},
+                annot_file,
+                images_path,
+            )
+
+    # for dataset_name, dataset_path in zip(original_config['DATASETS']["TEST"], original_config['DATASETS']["TEST_DIR"]):
+    #     print(f"Registering dataset {dataset_name} from {dataset_path}")
+    #     register_datasets(dataset_name, dataset_path)
     # train the model
     train_model(cfg)
 
