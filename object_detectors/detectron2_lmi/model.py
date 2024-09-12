18f3ea6ffee802471f3ba419a432724e0154114e
RugvedMavidipalli <rmavidipalli@gmail.com>
2024-09-11 23:15:59 -0400

    trt inference implimentation

--- object_detectors/detectron2_lmi/model.py
+++ object_detectors/detectron2_lmi/model.py
@@ -4,34 +4,31 @@ from cuda import cudart
 import numpy as np
 import detectron2_lmi.utils.common_runtime as common
 from typing import Optional
+from detectron2.structures import Instances, Boxes, ROIMasks
+import torch
+from gadget_utils.pipeline_utils import plot_one_box, resize_image
 
 class Detectron2TRT(ModelBase):
     
-    logger = trt.Logger(trt.Logger.INFO)
-    
     def __init__(self, model_path):
         
-        self.model_path = model_path
-        
+        # Load TRT engine
+        self.logger = trt.Logger(trt.Logger.INFO)
         trt.init_libnvinfer_plugins(self.logger, namespace="")
-        
-        # load the model
         with open(model_path, "rb") as f, trt.Runtime(self.logger) as runtime:
-            self.model = runtime.deserialize_cuda_engine(f.read())
-        
-        # create an execution context
-        self.context = self.model.create_execution_context()
-         
+            self.engine = runtime.deserialize_cuda_engine(f.read())
+        self.context = self.engine.create_execution_context()
+
         self.inputs = []
         self.outputs = []
         self.allocations = []
-        for i in range(self.model.num_io_tensors):
-            name = self.model.get_tensor_name(i)
+        for i in range(self.engine.num_io_tensors):
+            name = self.engine.get_tensor_name(i)
             is_input = False
-            if self.model.get_tensor_mode(name) == trt.TensorIOMode.INPUT:
+            if self.engine.get_tensor_mode(name) == trt.TensorIOMode.INPUT:
                 is_input = True
-            dtype = self.model.get_tensor_dtype(name)
-            shape = self.model.get_tensor_shape(name)
+            dtype = self.engine.get_tensor_dtype(name)
+            shape = self.engine.get_tensor_shape(name)
             if is_input:
                 self.batch_size = shape[0]
             size = np.dtype(trt.nptype(dtype)).itemsize
@@ -51,87 +48,89 @@ class Detectron2TRT(ModelBase):
                 self.inputs.append(binding)
             else:
                 self.outputs.append(binding)
-        self.input_shape, self.data_type = self.inputs[0]["shape"], self.inputs[0]["dtype"]
-        self.logger.log(trt.Logger.INFO, f"Input shape: {self.input_shape}")
-        self.logger.log(trt.Logger.INFO, f"Input: {self.inputs[0]}")
     
     def warmup(self):
+        """_summary_
+        """
         image_h, image_w = self.input_shape[2], self.input_shape[3]
         input = np.random.rand(self.batch_size, 3, image_h, image_w).astype(self.data_type)
         self.forward(input)
         
-    def preprocess(self, images):
-        input = np.zeros(self.input_shape, dtype=self.data_type)
-        # convert the image to channels first
+    def preprocess(self, images: list|np.ndarray):
+        """_summary_
+
+        Args:
+            images (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        image_h, image_w = self.input_shape[2], self.input_shape[3]
+        input = np.zeros((self.batch_size, 3, image_h, image_w), dtype=np.float32)
+        if isinstance(images, np.ndarray):
+            images = [images]
         for i, image in enumerate(images):
-            image = image.astype(np.float32)
-            # mean = (103.53, 116.28, 123.675)
-            # std = (1.0, 1.0, 1.0)
-            # image /= 255.0
-            # image -= mean
-            # image /= std
-            image = image.transpose(2, 0, 1)
-            input[i] = image
+            #HWC to CHW
+            # nomalization is handled in the model
+            input[i] = image.astype(np.float32).transpose(2, 0, 1)
         return input
     
-    def forward(self, input):
-        # prepare the outputs
-        predictions = []
-        for output in self.outputs:
-            predictions.append(np.zeros(output["shape"], dtype=output["dtype"]))
+    def forward(self, inps):
+        outputs = []
+        for shape, dtype in self.output_spec():
+            outputs.append(np.zeros(shape, dtype))
+        # Process I/O and execute the network.
         common.memcpy_host_to_device(
-            self.inputs[0]["allocation"], np.ascontiguousarray(input)
+            self.inputs[0]["allocation"], np.ascontiguousarray(inps)
         )
         self.context.execute_v2(self.allocations)
-        
-        # copy the outputs back to the host
-        for i, output in enumerate(self.outputs):
-            common.memcpy_device_to_host(predictions[i], output["allocation"])
-        return predictions
+        for o in range(len(outputs)):
+            common.memcpy_device_to_host(outputs[o], self.outputs[o]["allocation"])
+        return outputs
 
-    def postprocess(self, images, predictions):
-        pass
-            
-            
-    
-    def predict(self, images):
-        # preprocess the image
-        input = self.preprocess(images)
-        predictions = self.forward(input)
-        self.postprocess(images, predictions)
-        
+    def postprocess(self, image, predictions):
+        """_summary_
+
+        Args:
+            image (_type_): _description_
+            predictions (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        # assuming batch size is 1
+        instances = predictions[0][0]
+        boxes = predictions[1][0]
+        masks = predictions[4][0]
+        scores = predictions[2][0]
+        classes = predictions[3][0]
         results = {
-            "instances": predictions[0],
-            "boxes": predictions[1],
-            "scores": predictions[2],
-            "classes": predictions[3],
-            "masks": predictions[4],
+            "boxes": [],
+            "scores": [],
+            "classes": [],
+            "masks": []
         }
-        return results
+        image_h, image_w = image.shape[0], image.shape[1]
+        for i in range(0, instances):
+            y1, x1, y2, x2 = boxes[i]
+            x1, y1, x2, y2 = x1*image_w, y1*image_h, x2*image_w, y2*image_h
+            mask = masks[i]
+            results["boxes"].append([x1, y1, x2, y2])
+            results["scores"].append(scores[i])
+            results["classes"].append(classes[i])
+            w = x2 - x1 # width
+            h = y2 - y1 # height
+            mask = mask.astype(np.uint8)
+            mask = resize_image(mask, H=h, W=w)
+            # crop the mask to the bbox size
+            crop_mask = np.zeros((image_h, image_w), dtype=np.uint8)
+            crop_mask[round(y1):round(y2), round(x1):round(x2)] = mask
+            mask = crop_mask
+            results["masks"].append(mask)
+        return results 
             
-        
-        
-        
-        
-    
-        
-if __name__ == "__main__":
-    import argparse
-    import cv2
-    import time
-    
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--model_path", type=str, required=True)
-    parser.add_argument("--image_path", type=str, required=True)
-    args = parser.parse_args()
-    
-    model = Detectron2TRT(args.model_path)
-    model.warmup()
-    print(args.image_path)
-    image = cv2.imread(args.image_path)
-    # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    t0 = time.time()
-    output = model.predict([image])
-    print("Inference time: {:.2f} ms".format((time.time() - t0) * 1000))
-    for box in output["boxes"][0]:
-        print(box)
+    def predict(self, image):
+        input = self.preprocess(image)
+        predictions = self.forward(input)
+        results = self.postprocess(image, predictions)
+        return results
