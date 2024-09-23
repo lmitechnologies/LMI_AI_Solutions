a887f98f9fcce10f24118e82016a3e4c31bacf69
RugvedMavidipalli <rmavidipalli@gmail.com>
2024-09-23 11:57:35 -0400

    updated batch argument

--- object_detectors/detectron2_lmi/convert.py
+++ object_detectors/detectron2_lmi/convert.py
@@ -4,8 +4,8 @@ import subprocess
 import argparse
 
 def convert_to_onnx(**kwargs):
-    subprocess.run(f"python {os.path.join(os.path.dirname(os.path.realpath(__file__)), 'detectron2_lmi', 'converter/detectron2_to_onnx.py')}", shell=True)
-    subprocess.run(f"python {os.path.join(os.path.dirname(os.path.realpath(__file__)), 'detectron2_lmi', 'converter/detectron2_onnx_trtonnx.py')} -b {kwargs.get('batch_size', 1)}", shell=True)
+    subprocess.run(f"python {os.path.join(os.path.dirname(os.path.realpath(__file__)),'converter/detectron2_to_onnx.py')}", shell=True)
+    subprocess.run(f"python {os.path.join(os.path.dirname(os.path.realpath(__file__)),'converter/detectron2_onnx_trtonnx.py')} -b {kwargs.get('batch_size', 1)}", shell=True)
 
 def convert_to_trt(**kwargs):
     subprocess.run(f'trtexec --onnx={os.path.join(f"/home/weights/onnx", "exported.onnx")} --saveEngine={os.path.join(f"/home/weights/", "model.trt")} --useCudaGraph --fp16', shell=True)
