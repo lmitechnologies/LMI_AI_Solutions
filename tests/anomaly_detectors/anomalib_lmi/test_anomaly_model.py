6299dd6cf50e22dba6ef81400e6e9c8c1eb365dd
yijun <yijun1121@gmail.com>
2024-09-26 11:43:14 +0800

    f

--- tests/anomaly_detectors/anomalib_lmi/test_anomaly_model.py
+++ tests/anomaly_detectors/anomalib_lmi/test_anomaly_model.py
@@ -11,7 +11,7 @@ sys.path.append(os.path.join(ROOT, 'anomaly_detectors'))
 
 
 import gadget_utils.pipeline_utils as pipeline_utils
-from anomalib_lmi.anomaly_model2 import AnomalyModel2
+from anomalib_lmi.anomaly_model import AnomalyModel
 
 
 logging.basicConfig()
@@ -20,21 +20,21 @@ logger.setLevel(logging.DEBUG)
 
 
 DATA_PATH = 'tests/assets/images/nvtec-ad'
-MODEL_PATH = 'tests/assets/models/ad/model_v1.pt'
-OUTPUT_PATH = 'tests/assets/validation/ad_v1'
+MODEL_PATH = 'tests/assets/models/ad/model_v0.pt'
+OUTPUT_PATH = 'tests/assets/validation/ad_v0'
 
 
 
 def test_model():
-    AnomalyModel2.test(
+    AnomalyModel.test(
         MODEL_PATH, DATA_PATH, OUTPUT_PATH, generate_stats=True,annotate_inputs=True,
     )
     
     
 def test_convert():
     with tempfile.TemporaryDirectory() as t:
-        AnomalyModel2.convert(MODEL_PATH,t,fp16=True)
-        AnomalyModel2.test(
+        AnomalyModel.convert(MODEL_PATH,t,fp16=True)
+        AnomalyModel.test(
             os.path.join(t,'model.engine'), DATA_PATH, OUTPUT_PATH, generate_stats=True,annotate_inputs=True,
         )
     
