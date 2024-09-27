1e4bc6b2f1a7508c614ba88fbd98e58ef3d0d27c
yijun <yijun1121@gmail.com>
2024-09-27 14:27:09 +0800

    f

--- tests/anomaly_detectors/anomalib_lmi/test_anomaly_model2.py
+++ tests/anomaly_detectors/anomalib_lmi/test_anomaly_model2.py
@@ -1,7 +1,14 @@
 import logging
+from collections.abc import Sequence
 import sys
 import os
 import tempfile
+import glob
+import cv2
+import numpy as np
+import torch
+from anomalib.deploy.inferencers.torch_inferencer import TorchInferencer
+from anomalib.data.utils import read_image
 
 # add path to the repo
 PATH = os.path.abspath(__file__)
@@ -25,6 +32,33 @@ OUTPUT_PATH = 'tests/assets/validation/ad_v1'
 
 
 
+def test_compare_results_with_anomalib():
+    """
+    compare prediction results between current implementation and anomalib
+    """
+    model1 = TorchInferencer(MODEL_PATH)
+    model2 = AnomalyModel2(MODEL_PATH)
+    paths = glob.glob(os.path.join(DATA_PATH, '*.png'))
+    for p in paths:
+        tensor = read_image(p,as_tensor=True)
+        pred = model1.forward(model1.pre_process(tensor))
+        if isinstance(pred, dict):
+            pred = pred['anomaly_map']
+        elif isinstance(pred, Sequence):
+            pred = pred[1]
+        elif isinstance(pred, torch.Tensor):
+            pass
+        else:
+            raise Exception(f'Not supported output: {type(pred)}')
+        pred = pred.cpu().numpy()
+        
+        im = cv2.imread(p)
+        rgb = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+        pred2 = model2.predict(rgb)
+        
+        assert np.allclose(pred,pred2,rtol=0)
+        
+        
 def test_model():
     AnomalyModel2.test(
         MODEL_PATH, DATA_PATH, OUTPUT_PATH, generate_stats=True,annotate_inputs=True,
