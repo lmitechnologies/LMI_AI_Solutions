212545df98c50c6cdbf810f0ba3be60b34742cd2
RugvedMavidipalli <rmavidipalli@gmail.com>
2025-03-07 15:29:37 -0500

    updated json to yolo

--- lmi_utils/label_utils/json_to_yolo.py
+++ lmi_utils/label_utils/json_to_yolo.py
@@ -29,7 +29,7 @@ def args():
     args = vars(ap.parse_args())
     return args
 
-def write_txts(fname_to_rows, path_txts, fnames=None):
+def write_txts(fname_to_rows, path_txts, fnames,file_id_map):
     """
     write to the yolo format txts
     Arugments:
@@ -42,8 +42,12 @@ def write_txts(fname_to_rows, path_txts, fnames=None):
         if fnames is not None and fname not in fnames:
             continue
         name = os.path.basename(fname)
-        txt_file = os.path.join(path_txts, name)
+        out_name = name
         ext = name.split('.')[-1]
+        file_id = file_id_map[fname]
+        if f'id{file_id}_' not in out_name:
+            out_name = f'id{file_id}_{out_name}'
+        txt_file = os.path.join(path_txts, out_name)
         txt_file = txt_file.replace(f'.{ext}', '.txt')
     
         with open(txt_file, 'w') as f:
@@ -72,7 +76,7 @@ def update_file_dimensions(dataset, path_imgs):
         file.width = w
     return dataset
 
-def copy_images_in_folder(path_img, path_out, fnames=None):
+def copy_images_in_folder(path_img, path_out, fnames, file_id_map):
     """
     copy the images from one folder to another
     Arguments:
@@ -84,7 +88,11 @@ def copy_images_in_folder(path_img, path_out, fnames=None):
         raise Exception('fnames cannot be None')
     for fname in fnames:
         logger.info(f'copying {fname}')
-        shutil.copy(os.path.join(path_img, fname), path_out)
+        out_name = os.path.basename(fname)
+        file_id = file_id_map[fname]
+        if f'id{file_id}_' not in out_name:
+            out_name = f'id{file_id}_{out_name}'
+        shutil.copy(os.path.join(path_img, fname), os.path.join(path_out, out_name))
     
     logger.info(f'copied {len(fnames)} images to {path_out}')
 
@@ -121,6 +129,7 @@ def convert_to_yolo(args):
     
     train_dataset = Dataset.load(path_train_json)
     train_dataset = update_file_dimensions(train_dataset, path_train_imgs)
+    train_file_id_map = {f.path:f.id for f in train_dataset.files}
     
     train_yolo_dataset = train_dataset.to_yolo(
         merge_boxes=merge_box,
@@ -132,6 +141,7 @@ def convert_to_yolo(args):
     if use_train_for_val is False:
         val_dataset = Dataset.load(path_val_json)
         val_dataset = update_file_dimensions(val_dataset, path_val_imgs)
+        val_file_id_map = {f.path:f.id for f in val_dataset.files}
         val_yolo_dataset = val_dataset.to_yolo(
             merge_boxes=merge_box,
             to_segmentation=bbox_to_mask,
@@ -141,6 +151,7 @@ def convert_to_yolo(args):
         )
     else:
         val_yolo_dataset = train_yolo_dataset
+        val_file_id_map = train_file_id_map
     
     
     # path for labels files
@@ -153,15 +164,16 @@ def convert_to_yolo(args):
     train_files = list(train_yolo_dataset['image_labels'].keys())
     val_files = list(val_yolo_dataset['image_labels'].keys())
     
+    
     logger.info(f'train files: {len(train_files)}')
     logger.info(f'val files: {len(val_files)}')
     
 
-    write_txts(train_yolo_dataset['image_labels'], path_txts=path_txts_train, fnames=train_files)
+    write_txts(train_yolo_dataset['image_labels'], path_txts=path_txts_train, fnames=train_files, file_id_map=train_file_id_map)
     if not os.path.exists(path_out_imgs_train):
         os.makedirs(path_out_imgs_train)
     if len(val_files)>0 and use_train_for_val is False:
-        write_txts(val_yolo_dataset['image_labels'], path_txts=path_txts_val,fnames=val_files)
+        write_txts(val_yolo_dataset['image_labels'], path_txts=path_txts_val,fnames=val_files, file_id_map=val_file_id_map)
         if not os.path.exists(path_out_imgs_val):
             os.makedirs(path_out_imgs_val)
     
@@ -199,10 +211,10 @@ def convert_to_yolo(args):
         if len(val_files)>0 and use_train_for_val is False:
             val_fnames = [k for k in val_files]
     
-    copy_images_in_folder(path_img=path_train_imgs, path_out=path_out_imgs_train, fnames=train_fnames)
+    copy_images_in_folder(path_img=path_train_imgs, path_out=path_out_imgs_train, fnames=train_fnames, file_id_map=train_file_id_map)
     
     if len(val_fnames)>0 and use_train_for_val is False:
-        copy_images_in_folder(path_img=path_val_imgs, path_out=path_out_imgs_val, fnames=val_fnames)
+        copy_images_in_folder(path_img=path_val_imgs, path_out=path_out_imgs_val, fnames=val_fnames, file_id_map=val_file_id_map)
     
 
 def main(args):
