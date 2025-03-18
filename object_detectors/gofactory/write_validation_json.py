c50e2aff82d2b91ec88ed723b4a8cf4f2690ec14
Yijun <yijun1121@gmail.com>
2025-03-18 12:37:33 +0800

    init

--- object_detectors/gofactory/write_validation_json.py
+++ object_detectors/gofactory/write_validation_json.py
@@ -1,11 +1,10 @@
 import os
 import cv2
 import numpy as np
-import scipy.optimize
 import torch
 from ultralytics.utils.metrics import box_iou, mask_iou
-import scipy
 import logging
+import json
 
 from ultralytics_lmi.yolo.model import Yolo, YoloPose, YoloObb
 from dataset_utils.representations import Dataset, Annotation, AnnotationType, Box, Mask
@@ -16,35 +15,6 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-def match_predictions(preds, labels, cls_to_id, ious):
-    """match predictions to labels
-
-    Args:
-        preds (dict): a dictionary of model outputs
-        labels (dict): a dictionary of labels
-        cls_to_id (dict): a map <class_name : class_idx>
-        ious (numpy | None): a matrix of shape (N,M), where N is the number of labels, M is the number of predictions
-
-    Returns:
-        dict: a map <prediction_idx : label_idx>
-    """
-    if ious is None:
-        return {}
-    
-    pred_ids = np.array([cls_to_id[l] for l in preds['classes']])
-    label_ids = labels['ids']
-    class_match = label_ids[:,None] == pred_ids
-    ious = ious.cpu().numpy()
-    ious = ious*class_match # zero out ious for non-matching classes, (n_gt, n_pred)
-    
-    # Hungarian matching
-    gt_idx, pred_idx = scipy.optimize.linear_sum_assignment(ious, maximize=True)
-    valid = ious[gt_idx, pred_idx] > 0
-    gt_idx = gt_idx[valid]
-    pred_idx = pred_idx[valid]
-    pred_to_gt = {pred_idx[i]:gt_idx[i] for i in range(len(gt_idx))}
-    return pred_to_gt
-    
 
 def parse_annotations(annotations:list[Annotation], h:int, w:int):
     """parse label annotations from a list. Only support Box and Mask annotation objects.
@@ -80,7 +50,7 @@ def parse_annotations(annotations:list[Annotation], h:int, w:int):
     }
 
 
-def write_json(model_path, config_path, image_dir, label_path, output_path, confidence=0.01, max_det=600):
+def write_json(model_path, config_path, image_dir, label_path, out_pred_json, out_iou_dir, confidence=0.01, iou=0.45, max_det=600):
     """write predictions and labels to a json file
 
     Args:
@@ -89,7 +59,9 @@ def write_json(model_path, config_path, image_dir, label_path, output_path, conf
         image_dir (str): a input image directory, where each image should have the same dimension as training images
         label_path (str): a path to a label json file
         output_path (str): a full output json file path
+        out_iou_dir (str): a full output folder for iou matrix json files
         confidence (float, optional): a confidence threshold. Defaults to 0.01.
+        iou (float, optional): an iou threshold for NMS. Defaults to 0.45.
         max_det (int, optional): the max number of detections. Defaults to 600.
         
     """
@@ -109,22 +81,37 @@ def write_json(model_path, config_path, image_dir, label_path, output_path, conf
         im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
         h,w = im.shape[:2]
         labels = parse_annotations(file_annot.annotations, h, w)
-        preds,_ = model.predict(im, confidence, max_det=max_det, return_segments=False)
+        preds,_ = model.predict(im, confidence, iou=iou, max_det=max_det, return_segments=False)
         
         # get ious
         ious = None
         if 'masks' in preds:
-            if len(labels['masks']) and len(preds['masks']):
+            n_gt = len(labels['masks'])
+            n_pred = len(preds['masks'])
+            if n_gt and n_pred:
                 gt_masks = torch.from_numpy(labels['masks']).float().to(model.device)
                 pred_masks = torch.from_numpy(preds['masks']).to(model.device)
                 ious = mask_iou(gt_masks.view(gt_masks.shape[0], -1),pred_masks.view(pred_masks.shape[0],-1))
         else:
-            if len(labels['boxes']) and len(preds['boxes']):
+            n_gt = len(labels['boxes'])
+            n_pred = len(preds['boxes'])
+            if n_gt and n_pred:
                 gt_boxes = torch.from_numpy(labels['boxes'][:,:-1]).to(model.device)
                 pred_boxes = torch.from_numpy(preds['boxes']).to(model.device)
                 ious = box_iou(gt_boxes, pred_boxes)
-            
-        pred_to_gt = match_predictions(preds, labels, cls_to_id, ious)
+                
+        # write ious to a json file
+        ious_out = [] if ious is None else ious.cpu().numpy().tolist()
+        iou_json = dict(
+            impath=p,
+            n_gt=n_gt,
+            n_pred=n_pred,
+            iou=ious_out
+        )
+        os.makedirs(out_iou_dir, exist_ok=True)
+        out_iou_path = os.path.join(out_iou_dir, file_annot.id + '.json')
+        with open(out_iou_path, 'w') as f:
+            json.dump(iou_json, f)
         
         # add predictions to dataset
         logger.info(f'Found {len(preds["classes"])} predictions for {fname}')
@@ -134,30 +121,24 @@ def write_json(model_path, config_path, image_dir, label_path, output_path, conf
             label = preds['classes'][i]
             label_id = cls_to_id[label]
             score = preds['scores'][i].item()
-            label_annot_id = None
-            iou = 0
-            if i in pred_to_gt:
-                gt_id = pred_to_gt[i]
-                iou = ious[gt_id, i].item()
-                label_annot_id = file_annot.annotations[gt_id].id
             
             if mask is not None:
                 dt = dict(
                     id=str(pred_annot_id), label_id=str(label_id), type=AnnotationType.MASK, value=Mask(mask), 
-                    link=label_annot_id, confidence=score, iou=iou
+                    confidence=score, 
                 )
                 file_annot.predictions.append(Annotation(**dt))
                 pred_annot_id += 1
             else:
                 dt = dict(
                     id=str(pred_annot_id), label_id=str(label_id), type=AnnotationType.BOX, value=Box(*box,angle=0), 
-                    link=label_annot_id, confidence=score, iou=iou
+                    confidence=score
                 )
                 file_annot.predictions.append(Annotation(**dt))
                 pred_annot_id += 1
                 
     # write out dataset
-    dataset.save(output_path)
+    dataset.save(out_pred_json)
     return
 
 
@@ -168,10 +149,12 @@ if __name__ =='__main__':
     parser.add_argument('--config_path',default=None,help='[optional] a path to a model config file')
     parser.add_argument('--img_dir',required=True,help='a input image directory')
     parser.add_argument('--label_path',required=True,help='a path to a label json file')
-    parser.add_argument('--out_path',required=True,help='a full path to a output json file')
+    parser.add_argument('--out_pred_json',required=True,help='a full output json file path for predictions and labels')
+    parser.add_argument('--out_iou_dir',required=True,help='a full output folder for saving iou json files')
     parser.add_argument('--confidence',default=0.01,type=float,help='[optional] confidence threshold, defaults to 0.01')
+    parser.add_argument('--iou',default=0.45,type=float,help='[optional] iou NMS threshold, defaults to 0.45')
     parser.add_argument('--max_det',default=600,type=int,help='[optional] the max number of detections per image, default to 600')
     ap = parser.parse_args()
     
-    write_json(ap.model_path, ap.config_path, ap.img_dir, ap.label_path, ap.out_path, ap.confidence, ap.max_det)
+    write_json(ap.model_path, ap.config_path, ap.img_dir, ap.label_path, ap.out_pred_json, ap.out_iou_dir, ap.confidence, ap.iou, ap.max_det)
     
