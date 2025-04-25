13875330ca7afe2ac701d723654bdec5f17250c2
RugvedMavidipalli <rmavidipalli@gmail.com>
2025-04-25 12:04:12 -0400

    updated od core and model.py classes

--- object_detectors/od_core/object_detector.py
+++ object_detectors/od_core/object_detector.py
@@ -1,12 +1,23 @@
 from typing import Dict, Any
 from .od_base import ODBase
 from .object_detector_registry import ObjectDetectorRegistry
+import logging
 
 class ObjectDetector(ODBase):
 
     def __new__(cls, metadata: Dict[str, Any], *args, **kwargs):
-        model_path = metadata.get('model_path') 
-
+        logger = logging.getLogger(__name__)
+        model_path = metadata.get('model_path')
+        image_size = metadata.get('image_size')
+        if image_size:
+            if 'image_size' in kwargs and kwargs['image_size'] is not None:
+                logger.warning(
+                    "Both 'image_size' in metadata and kwargs provided. "
+                    "Using the one from metadata."
+                )
+            
+        kwargs['image_size'] = image_size
+        
         try:
             wrapper_cls = ObjectDetectorRegistry.get_class(metadata)
         except ValueError as e:
