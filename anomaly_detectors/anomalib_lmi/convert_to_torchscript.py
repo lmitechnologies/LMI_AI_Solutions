0342a5acb4bc871c780fce871f1846fa8287751d
rugved.mavidipalli <rugved.mavidipalli@lmitechnologies.com>
2025-05-06 15:04:45 -0400

    created a torchscript

---
+++ anomaly_detectors/anomalib_lmi/convert_to_torchscript.py
@@ -0,0 +1,59 @@
+import logging 
+import torch
+from .base import to_list
+
+logging.basicConfig()
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def generate_traced_torchscript(model_path,output_path,version='v1'):
+    """
+    Generate a traced TorchScript model from the given model path.
+    
+    Args:
+        model_path (str): Path to the model file.
+        version (str): Version of the model. Default is 'v1'.
+        
+    Returns:
+        torch.jit.ScriptModule: The traced TorchScript model.
+    """
+    if version == 'v1':
+        return convert_v1_torchscript(model_path=model_path, output_path=output_path)
+    else:
+        raise ValueError(f"Unsupported version: {version}")
+
+def convert_v1_torchscript(model_path, output_path):
+    """
+    Convert a model to TorchScript format.
+    
+    Args:
+        model_path (str): Path to the model file.
+        
+    Returns:
+        torch.jit.ScriptModule: The converted TorchScript model.
+    """
+    from torchvision.transforms import v2
+    ckpt = torch.load(model_path)
+    model = ckpt['model'].eval().cuda()
+    image_size = None
+    for d in model.transform.transforms:
+        if isinstance(d, v2.Resize):
+            image_size = to_list(d.size)
+    inp = torch.rand(1,3,image_size[0], image_size[1]).cuda()
+    traced_model = torch.jit.trace(model,inp,strict=False)
+    torch.jit.save(traced_model, output_path)
+    return traced_model
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser(description="Convert a model to TorchScript format.")
+    parser.add_argument('--input_path', type=str, required=True, help='Path to the model file.')
+    parser.add_argument('--output_path', type=str, required=True, help='Path to save the converted model.')
+    parser.add_argument('--version', type=str, default='v1', help='Version of the model. Default is v1.',required=False, choices=['v1'])
+    
+    args = parser.parse_args()
+    
+    generate_traced_torchscript(args.model_path, args.output_path, args.version)
+
+if __name__ == "__main__":
+    main()
