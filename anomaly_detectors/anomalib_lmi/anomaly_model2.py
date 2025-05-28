8a10ea216df448ad54682f74714c1de098ab9b5f
rugved.mavidipalli <rugved.mavidipalli@lmitechnologies.com>
2025-05-28 15:08:55 -0400

    updated utils

--- anomaly_detectors/anomalib_lmi/anomaly_model2.py
+++ anomaly_detectors/anomalib_lmi/anomaly_model2.py
@@ -244,6 +244,7 @@ class AnomalyModel2(Anomalib_Base):
                 aggregated_output_tensor = self.bindings['output'].data
 
             elif self.inference_mode == 'PT':
+                self.logger.info(f'Running PT model inference on input batch of shape: {input_batch.shape}')
                 preds = self.pt_model(input_batch)
                 if isinstance(preds, torch.Tensor):
                     aggregated_output_tensor = preds
