35e46450197af14505230f535015a12dc24d8fd0
rugved.mavidipalli <rugved.mavidipalli@lmitechnologies.com>
2025-07-03 14:12:23 -0400

    updated

--- anomaly_detectors/anomalib_lmi/anomaly_model2.py
+++ anomaly_detectors/anomalib_lmi/anomaly_model2.py
@@ -130,6 +130,7 @@ class AnomalyModel2(Anomalib_Base):
         args:
             - image: numpy array [H,W,Ch]
         '''
+        
         img = self.from_numpy(image).float()
         
         # grayscale to rgb
@@ -139,14 +140,21 @@ class AnomalyModel2(Anomalib_Base):
         img = img.permute((2, 0, 1)).unsqueeze(0)
         img = img / 255.0
         
+        
         if self.tiler is not None:
             img = self.tiler.tile(img,self.tile_mode)
         
-        # resize baked into the pt model
+        
         batch = img.shape[0]
         if self.inference_mode=='TRT' and batch != self.batch_size:
             self.logger.warning(f'Got batch size of {batch},  but trt expects {self.batch_size}. The trt engine might output weird results')
             img = F.interpolate(img, size=self.model_shape, mode='bilinear')
+        
+        # resize baked into the pt model (although some torchscript models dont have preprocessing so resizing here)
+        if self.tiler is None and self.inference_mode == 'PT':
+            if img.shape[1] != self.model_shape[0] or img.shape[2] != self.model_shape[1]:
+                self.logger.debug(f'Input image shape {image.shape[:2]} does not match model shape {self.model_shape}. Resizing input image.')
+                img = v2.Resize(self.model_shape, antialias=True)(img)
         
         img = img.contiguous()
         return img.half() if self.fp16 else img
