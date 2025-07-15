660b5fa72283c20c5e52cf5c9a49e022542bdb36
rugved.mavidipalli <rugved.mavidipalli@lmitechnologies.com>
2025-07-15 09:33:33 -0400

    updated ;

--- object_detectors/detectron2_lmi/model.py
+++ object_detectors/detectron2_lmi/model.py
@@ -485,10 +485,10 @@ class Detectron2PT(ODBase):
         for idx, image_preds in enumerate(predictions):
             image_h, image_w = images[idx].shape[:2]
             if image_preds is None or len(image_preds['pred_classes']) == 0:
-                results["boxes"].append(np.empty((0, 4), dtype=np.float32))
-                results["scores"].append(np.empty((0,), dtype=np.float32))
-                results["classes"].append(np.empty((0,), dtype=np.int64))
-                results["masks"].append(np.empty((0, 0, 0), dtype=np.float32))
+                results["boxes"].append([])
+                results["scores"].append([])
+                results["classes"].append([])
+                results["masks"].append([])
                 results["segments"].append([])
                 continue
             
