34a8f43ad1ea4311ebb7ed4355835415c5779f06
Yijun <yijun1121@gmail.com>
2025-07-17 21:57:27 +0800

    update

--- object_detectors/od_core/__init__.py
+++ object_detectors/od_core/__init__.py
@@ -1,4 +1,4 @@
-from ultralytics_lmi.yolo.model import *
-from detectron2_lmi.model import *
-from yolov8_lmi.model import *
+from ultralytics_lmi.yolo.model import Yolo, YoloObb, YoloPose
+from detectron2_lmi.model import Detectron2Model, Detectron2PT, Detectron2TRT
+from yolov8_lmi.model import Yolov8, Yolov8Obb, Yolov8Pose
 from yolov5_lmi.model import Yolov5
