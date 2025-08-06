86e40ca347ce40b2a2a60e708f5609c762c355eb
Yijun Jiang <46984040+nickjyj@users.noreply.github.com>
2025-08-06 09:55:42 +0800

    avoid circular import issue for AD

--- anomaly_detectors/ad_core/__init__.py
+++
@@ -1,2 +0,0 @@
-from anomalib_lmi.anomaly_model import *
-from anomalib_lmi.anomaly_model2 import *
--- anomaly_detectors/ad_core/anomaly_detector_registry.py
+++ anomaly_detectors/ad_core/anomaly_detector_registry.py
@@ -1,6 +1,18 @@
 import json
 from typing import Type, Dict, Tuple, Any, Optional, List
 import logging
+import importlib
+import pkgutil
+
+
+PACKAGES = ["anomalib_lmi", "ad_core"]
+TARGET_MODULE_SUFFIXES = ['.anomaly_model','.anomaly_model2'] # Target suffixes to look for in the packages
+
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
 
 class AnomalyDetectorRegistry:
     _registry: Dict[Tuple[str, str, str, str, str], Type] = {}
@@ -18,7 +30,6 @@ class AnomalyDetectorRegistry:
 
     @classmethod
     def register(cls, metadata: Dict[str, Any]):
-        logger = logging.getLogger(__name__)
         logger.debug(f"Registering class with metadata: {metadata}")
         frameworks: Optional[List[str]] = metadata.get('frameworks')
         model_names: Optional[List[str]] = metadata.get('model_names')
@@ -46,7 +57,7 @@ class AnomalyDetectorRegistry:
                                     f"Combination already registered: "
                                     f"framework='{framework}', model_name='{model_name}', "
                                     f"task='{task}', version='{version}', info='{json.dumps(info, sort_keys=True)}' "
-                                    f"points to {existing_cls.__name__}. Cannot re-register with {wrapper_cls.__name__}."
+                                    f"points to {existing_cls.__module__}. Cannot re-register with {wrapper_cls.__module__}."
                                 )
                             cls._registry[key] = wrapper_cls
             return wrapper_cls
@@ -78,3 +89,24 @@ class AnomalyDetectorRegistry:
             )
 
         return wrapper_cls
+
+    @classmethod
+    def auto_register_models(cls):
+        """
+        Dynamically discovers and imports models to trigger registration.
+        """
+        
+        logger.info("Starting auto-discovery of anomaly detector models...")
+        for package_name in PACKAGES:
+            package = importlib.import_module(package_name)
+            package_path = package.__path__
+
+            # search for target module in each package
+            for _, module_name, _ in pkgutil.walk_packages(package_path, package_name + '.'):
+                if any(module_name.endswith(target) for target in TARGET_MODULE_SUFFIXES):
+                    try: 
+                        importlib.import_module(f"{module_name}")
+                        logger.info(f"Successfully imported {module_name}")
+                    except ImportError as e:
+                        logger.warning(f"Failed to import {module_name}: {e}")
+                        
