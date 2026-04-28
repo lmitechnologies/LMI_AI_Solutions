e3d3507725bfac034b45d016dca7235536fcab7e
Yijun <yijun1121@gmail.com>
2026-04-28 17:35:40 +0800

    remove default v0

--- lmi_utils/pipeline_base/pipeline_base.py
+++ lmi_utils/pipeline_base/pipeline_base.py
@@ -104,7 +104,7 @@ class PipelineBase(metaclass=ABCMeta):
 
         # Default to v1, but downgrade to v0 for specific cases
         meta_copy.setdefault("version", "v1")
-        if (meta_copy["package"] == "detectron2") or (model_type == "classification"):
+        if meta_copy["package"] == "detectron2":
             meta_copy["version"] = "v0"
 
         model_classes: Dict[str, Type] = {
